d4b6ebe626d8de85a491377926e8755aea616474
animeshb27 <abalse27@gmail.com>
2019-04-16 16:38:53 -0400

    Removed Docker Compose files

--- configuration/php/admin/js/tmx.initialization.js
+++
@@ -1,9 +0,0 @@
-// Connection specifics
-// 	devices: 	Must be 1 dimentional arra with "TEST" only
-// 	op: 		Address of Websocket Server to connect
-// 	port: 		Port on which to connect to the Web Socket
-// 	led: 		id of the element that contains the status indicator. 
-var connections = 	[
-				{ip:"127.0.0.1", port:"19760", led:"Logo"}
-			];
-
