221a70c4188ed9ad42e398d3e01ef7a05cfdae5e
anjan rayamajhi <anjan.rayamajhi@leidos.com>
2019-09-18 10:30:23 -0400

    Webservice ready for development and cleaned Pedestrian Plugin off object/library files

--- setupdhcp.sh
+++
@@ -1,6 +0,0 @@
-sudo service isc-dhcp-server stop
-sudo ifconfig eno1 55.55.55.1/24
-sudo service isc-dhcp-server start
-
-
-
--- stopdhcp.sh
+++
@@ -1,4 +0,0 @@
-sudo ifconfig down eno1
-sudo ifconfig up eno1
-
-sudo service isc-dhcp-server stop
