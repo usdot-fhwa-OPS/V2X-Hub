9b68a1bcad36f2f2b208c2335dac809fdd12bbbd
Animesh Balse <41792325+animeshb27@users.noreply.github.com>
2019-12-24 14:37:30 -0500

    Update TimPlugin.cpp

--- src/v2i-hub/TimPlugin/src/TimPlugin.cpp
+++ src/v2i-hub/TimPlugin/src/TimPlugin.cpp
@@ -2,7 +2,6 @@
 #include <WGS84Point.h>
 #include "TimeHelper.h"
 #include "XmlCurveParser.h"
-#include "VehicleLocate.h"
 #include <tmx/messages/IvpDmsControlMsg.h>
 #include <tmx/j2735_messages/TravelerInformationMessage.hpp>
 
@@ -47,10 +46,7 @@ void TimPlugin::UpdateConfigSettings() {
 		if (GetConfigValue<string>("MapFile", _mapFile))
 			_isMapFileNew = true;
 	}
-
-	GetConfigValue<uint64_t>("Snap Interval", _snapInterval);
-	GetConfigValue<uint64_t>("Vehicle Timeout", _vehicleTimeout);
-
+	
         std::lock_guard<mutex> lock(_cfgLock);
 	GetConfigValue("Start_Broadcast_Date", _startDate);
 	GetConfigValue("Stop_Broadcast_Date", _stopDate);
@@ -61,56 +57,6 @@ void TimPlugin::UpdateConfigSettings() {
 void TimPlugin::OnConfigChanged(const char *key, const char *value) {
 	PluginClient::OnConfigChanged(key, value);
 	UpdateConfigSettings();
-}
-
-
-void TimPlugin::HandleBasicSafetyMessage(BsmMessage &msg,
-		routeable_message &routeableMsg) {
-
-PLOG(logDEBUG)<<"HandleBasicSafetyMessage";
-	auto bsm = msg.get_j2735_data();
-
-	int regionNumber;
-	float speed_mph;
-	int32_t vehicleId;
-
-	if (VehicleLocate::ProcessBsmMessage(*bsm, &_tim, &regionNumber, &speed_mph, &vehicleId))
-	{
-		// Store the ID in a map, where the vehicle ID is the key, and the value is the speed
-		// and zone/region where the vehicle is currently at.
-		UpdateVehicleInZone(vehicleId, regionNumber, speed_mph);
-
-		// Get the highest priority id for all vehicles that are speeding.
-		int zoneId = GetHighestPriorityZoneId(_speedLimit);
-
-		PLOG(logDEBUG)<<"Highest Priority Zone: " << zoneId;
-
-		// Message ID is the ID of the message to display on the sign.
-		// An ID of 0 blanks the sign.
-		int msgId = 0;
-
-		// Only show a message on the sign when in zones 1, 2, or 3.
-		// Always show the same message (ID 3).  Otherwise the sign is blank.
-		if (zoneId >= 1 && zoneId <= 3)
-			msgId = 3;
-
-		if (_lastMsgIdSent != msgId)
-		{
-			PLOG(logDEBUG)<<"Sending DMS Message ID: " <<msgId;
-			_lastMsgIdSent = msgId;
-
-			IvpMessage *actionMsg = ivpDmsCont_createMsg(msgId);
-			if (actionMsg!=NULL)
-			{
-				ivp_broadcastMessage(_plugin, actionMsg);
-				ivpMsg_destroy(actionMsg);
-
-				PluginUtil::SetStatus<int>(_plugin, "DMS Message ID Sent", msgId);
-			}
-		}
-
-		SetStatusForVehiclesInZones();
-	}
 }
 
 void TimPlugin::OnStateChange(IvpPluginState state) {
@@ -249,119 +195,6 @@ void TimPlugin::TestFindRegion()
 		int regionNumber = VehicleLocate::FindRegion(&_tim, point, 0);
 		std::cout << "In Region: " << regionNumber << std::endl;
 	}
-}
-
-void TimPlugin::UpdateVehicleInZone(int32_t vehicleId, int zoneId, float speed_mph)
-{
-	uint64_t now = TimeHelper::GetMsTimeSinceEpoch();
-
-	// If msgId is 0, the vehicle is removed from the map.
-	// If msgId is not 0, the vehicle is either added or updated in the map.
-
-	std::map<int32_t, ZoneInfo>::iterator it = _vehicleInZone.find(vehicleId);
-	if (it == _vehicleInZone.end())
-	{
-		if (zoneId != 0)
-		{
-			PLOG(logDEBUG) << "Adding   Vehicle ID: " << vehicleId << ", zone: " << zoneId << ", speed: " << speed_mph;
-
-			ZoneInfo zoneInfo;
-			zoneInfo.ZoneId = zoneId;
-			zoneInfo.Speed_mph = speed_mph;
-			zoneInfo.LastUpdateTime = now;
-
-			_vehicleInZone.insert(std::pair<int32_t, ZoneInfo>(vehicleId, zoneInfo));
-		}
-	}
-	else
-	{
-		pthread_mutex_lock(&_settingsMutex);
-		uint64_t snapInterval = _snapInterval;
-		pthread_mutex_unlock(&_settingsMutex);
-
-		if (zoneId == 0 && (now - it->second.LastUpdateTime) < snapInterval)
-		{
-			PLOG(logDEBUG) << "Removing Vehicle ID: " << vehicleId << ", zone: " << zoneId << ", speed: " << speed_mph;
-			_vehicleInZone.erase(vehicleId);
-		}
-		else
-		{
-			if (it->second.ZoneId != zoneId || it->second.Speed_mph != speed_mph)
-			{
-				PLOG(logDEBUG) << "Updating Vehicle ID: " << vehicleId << ", zone: " << zoneId << ", speed: " << speed_mph;
-				it->second.ZoneId = zoneId;
-				it->second.Speed_mph = speed_mph;
-			}
-			it->second.LastUpdateTime = now;
-		}
-	}
-}
-
-// Remove any vehicles where a BSM has not been received within the timeout period.
-void TimPlugin::RemoveOldVehicles()
-{
-	uint64_t now = TimeHelper::GetMsTimeSinceEpoch();
-
-	pthread_mutex_lock(&_settingsMutex);
-	uint64_t vehicleTimeout = _vehicleTimeout;
-	pthread_mutex_unlock(&_settingsMutex);
-
-	std::map<int32_t, ZoneInfo>::iterator it = _vehicleInZone.begin();
-
-	while (it != _vehicleInZone.end())
-	{
-	    if (now - it->second.LastUpdateTime > vehicleTimeout)
-	    {
-	    	std::map<int32_t, ZoneInfo>::iterator toErase = it;
-	       it++;
-	       _vehicleInZone.erase(toErase);
-	    }
-	    else
-	    {
-	       it++;
-	    }
-	}
-}
-
-// Get the zone id with the highest priority of all vehicles that are speeding.
-int TimPlugin::GetHighestPriorityZoneId(unsigned int speedLimit_mph)
-{
-	// Remove any old vehicles that have timed out.
-	RemoveOldVehicles();
-
-	// The lower the zone id, the higher the priority.
-
-	int id = 9999;
-
-	for (auto& kv : _vehicleInZone)
-	{
-		if (kv.second.ZoneId != 0 && id > kv.second.ZoneId && kv.second.Speed_mph > speedLimit_mph)
-			id = kv.second.ZoneId;
-	}
-
-	return id == 9999 ? 0 : id;
-}
-
-void TimPlugin::SetStatusForVehiclesInZones()
-{
-	ostringstream ss;
-	ss.precision(1);
-
-	bool first = true;
-	for (auto& kv : _vehicleInZone)
-	{
-		if (first)
-			first = false;
-		else
-			ss << ", ";
-
-		ss << "[ID: " << kv.first << ", Zone: " << kv.second.ZoneId << ", Speed: " << fixed << kv.second.Speed_mph << "]";
-	}
-
-	if (first)
-		ss << "None";
-
-	SetStatus<string>("Vehicles In Zones", ss.str());
 }
 
 int TimPlugin::Main() {
