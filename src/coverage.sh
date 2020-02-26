a0d44ba98685bf7deeb2abf238022ba47dcc333b
jtbaird <john.baird@leidos.com>
2020-02-26 10:55:06 -0500

    changed gcovr to only report on tested plugins
    changed scope to preemption and pedestrian
    
    changed gcov options
    
    updated coverage report path

--- src/coverage.sh
+++ src/coverage.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
-
+set -x
 for d in v2i-hub/*
 do
     echo ""
     echo $d
     if [[ -d $d ]]; then
-        TESTS=`ls $d | grep run[a-zA-Z]*Tests`
-        echo $TESTS
-        cd $d
-        if $TESTS; then
-            ./$TESTS
+        if ls $d | grep run[a-zA-Z]*Tests ; then
+            TESTS="./`ls $d | grep run[a-zA-Z]*Tests`"
+            echo "$TESTS built"
+            cd $d
+            $TESTS         
+            cd ../..
+        else
+            echo "no tests built"
         fi
-        gcovr -k -r .
-        mv *.gcov ../../../coverage_reports/gcov
-        cd ../..
     fi
 done
+
+cd v2i-hub
+pwd
+gcovr -k ./P*
+mv *.gcov ../../coverage_reports/gcov
