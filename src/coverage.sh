c8b94a549a418603bca1c9856395e5ccf68e5143
jtbaird <john.baird@leidos.com>
2020-02-27 09:49:41 -0500

    added license headers and changed step labels

--- src/coverage.sh
+++ src/coverage.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+#  Copyright (C) 2018-2020 LEIDOS.
+# 
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+#  use this file except in compliance with the License. You may obtain a copy of
+#  the License at
+# 
+#  http://www.apache.org/licenses/LICENSE-2.0
+# 
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations under
+#  the License.
+
+# script to run tests, generate test-coverage, and store coverage reports in a place
+# easily accessible to sonar. Test names should follow convention run<pluginName>Tests
+
 set -x
 for d in v2i-hub/*
 do
