12a6547b3c7f02834e43c1227318911bb34064b1
jtbaird <john.baird@leidos.com>
2020-03-25 16:04:10 -0400

    added password validity check to init scripts

--- configuration/arm64/initialization.sh
+++ configuration/arm64/initialization.sh
@@ -3,9 +3,24 @@
 set -e
 
 # takes user-provided username and password for docker-compose.yml and stores in .env file
+USERNAME=$1
+PASSWORD=$2
 
-sudo echo "username=$1" > .env
-sudo echo "password=$2" >> .env
+if [[ -z $PASSWORD ]]; then
+  echo "No password entered."
+else
+  PASS_LENGTH=`echo $1 | wc -c`
+
+  if [ $PASS_LENGTH -ge 8 ] && echo $1 | grep [a-z] && echo $1 | grep [A-Z] && echo $1 | grep [0-9] && echo $1 | grep [\$\!\.\+_-\*@\#\^%\?~]; then
+      sudo echo "username=$1" > .env
+      sudo echo "password=$2" >> .env
+      sudo echo "VALID PASSWORD"
+  else
+      sudo echo "INVALID PASSWORD"
+      sudo echo "Password must be 8-12 charcters, and contain at least one of each of the following: uppercase letter, lowercase letter, number, and symbol"
+      sudo exit 1
+  fi
+fi
 
 # Max query attempts before consider setup failed
 MAX_TRIES=10
