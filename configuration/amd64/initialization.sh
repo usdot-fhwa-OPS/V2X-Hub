7f4b011e2aa6d2809007e56077f56b883dd4f1d8
jtbaird <john.baird@leidos.com>
2020-03-25 12:21:46 -0400

    adding init script for amd64 devices

---
+++ configuration/amd64/initialization.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get -y remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io 
+sudo apt -y install python3-pip
+sudo pip3 install docker-compose
+sudo docker-compose up -d
