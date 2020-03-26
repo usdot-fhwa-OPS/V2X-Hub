24b9d68c755c0cf4c82844457a5a99f7444c158e
Animesh Balse <41792325+animeshb27@users.noreply.github.com>
2020-03-26 13:36:47 -0400

    Update install_db.sh

--- configuration/amd64/mysql/install_db.sh
+++ configuration/amd64/mysql/install_db.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-USER=`head -1 .env | cut -d "=" -f 2`
-PASS=`tail -1 .env | cut -d "=" -f 2`
+USER=`head -1 ./.env | cut -d "=" -f 2`
+PASS=`tail -1 ./.env | cut -d "=" -f 2`
 
 mysql -uroot -pivp -e "INSERT INTO IVP.user (IVP.user.username, IVP.user.password, IVP.user.accessLevel) VALUES('$USER', '$PASS', 3)"
