74c6fa09fce64da83e07f6b8b50fe47cab1f536c
Anjan Rayamajhi <arayama@clemson.edu>
2020-12-15 11:12:09 -0500

    initial version of validation tool

---
+++ tools/validation-tool/appval.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+
+import J2735 
+import json
+import sys 
+import subprocess 
+
+
+### Read in the json file and enable scp to transfer it 
+### 
+
+username="test"
+dutip=""
+password="test"
+remotepath=""
+filenamepath=""
+#noIssue="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+#process=subprocess.Popen(['scp',filenamepath,username+'@'+dutip+':'+remotepath],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+regex='s/[ \t].*//;/^\(lo\|\)$/d'
+oifconfig = subprocess.Popen(['ifconfig','-a'],stdout = subprocess.PIPE)
+osed = subprocess.Popen(['sed',regex],stdin= oifconfig.stdout,stdout = subprocess.PIPE)
+endofline = osed.stdout
+
+#print("ifconfig -a |  sed 's/[ \t].*//;/^\(lo\|\)$/d'")
+
+for i in endofline:
+    print('\n',i.strip())
+
