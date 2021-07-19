4551c933b62c7430d076a4a4591d2a74e6f9548b
Saikrishna Bairamoni <84093461+SaikrishnaBairamoni@users.noreply.github.com>
2021-07-19 17:37:54 -0400

    Update CARMACloudPlugin.cpp

--- src/v2i-hub/CARMACloudPlugin/src/CARMACloudPlugin.cpp
+++ src/v2i-hub/CARMACloudPlugin/src/CARMACloudPlugin.cpp
@@ -22,7 +22,7 @@ CARMACloudPlugin::CARMACloudPlugin(string name) :PluginClient(name) {
         std::lock_guard<mutex> lock(_cfgLock);
 		GetConfigValue<string>("WebServiceIP",webip);
 		GetConfigValue<uint16_t>("WebServicePort",webport);
-		GetConfigValue<uint16_t>("fetchTime",fetchTime);
+		GetConfigValue<uint16_t>("fetchtime",fetchtime);
 		AddMessageFilter < tsm4Message > (this, &CARMACloudPlugin::HandleCARMARequest);
 
 		// Subscribe to all messages specified by the filters above.
@@ -65,7 +65,7 @@ void CARMACloudPlugin::HandleCARMARequest(tsm4Message &msg, routeable_message &r
 	       	strcpy(bounds_str,"");	
 		
 		//  get current time 
-		std::time_t tm = std::time(0)/60-fetchTime*24*60; //  T minus 24 hours in  min  
+		std::time_t tm = std::time(0)/60-fetchtime*24*60; //  T minus 24 hours in  min  
 
 		while(cnt<totBounds)
 		{
