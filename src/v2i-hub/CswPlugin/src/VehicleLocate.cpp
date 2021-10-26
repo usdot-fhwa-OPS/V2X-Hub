168f90e020a90b5c9279e16deafb4d42a530943c
Saikrishna Bairamoni <84093461+SaikrishnaBairamoni@users.noreply.github.com>
2021-10-26 16:39:17 -0400

    Update VehicleLocate.cpp

--- src/v2i-hub/CswPlugin/src/VehicleLocate.cpp
+++ src/v2i-hub/CswPlugin/src/VehicleLocate.cpp
@@ -76,6 +76,8 @@ int VehicleLocate::FindRegion(TiDataFrame *frame, WGS84Point point, uint16_t hea
 		WGS84Point* points = GetPointArray(nodes, nodesLength, nodesAnchor);
 
 		if (IsInPointList(points, nodesLength, point, heading, laneWidth/100.0))
+			free(points);
+			points = NULL;
 			return i + 1;
 
 		free(points);
