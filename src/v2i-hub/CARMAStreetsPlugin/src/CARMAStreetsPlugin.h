e414bc5b90aab56d9d67a6fcb16070d49b9d658c
dan-du-car <62157949+dan-du-car@users.noreply.github.com>
2021-11-15 10:49:26 -0500

    mobility operation message encoding/decoding for streets support (#271)
    * update
    
    * add mobilityoperation and bsm
    
    * update
    
    * update
    
    * update bsm id
    
    * update
    
    * update trajectory format
    
    * update
    
    * address comment
    
    * update tag
    
    * update
    
    * PR cmt
    
    * PR cmt
    
    * PR cmt
    
    * address comments

--- src/v2i-hub/CARMAStreetsPlugin/src/CARMAStreetsPlugin.h
+++ src/v2i-hub/CARMAStreetsPlugin/src/CARMAStreetsPlugin.h
@@ -9,6 +9,9 @@
 #include <tmx/TmxException.hpp>
 #include <tmx/j2735_messages/BasicSafetyMessage.hpp>
 #include "jsoncpp/json/json.h"
+#include <pthread.h>
+#include <boost/thread.hpp>
+#include <mutex>
 
 
 
@@ -34,20 +37,35 @@ protected:
 	void OnConfigChanged(const char *key, const char *value);
 
 	void OnStateChange(IvpPluginState state);
-
 	void HandleMobilityOperationMessage(tsm3Message &msg, routeable_message &routeableMsg);
 	void HandleMobilityPathMessage(tsm2Message &msg, routeable_message &routeableMsg);
+	void HandleBasicSafetyMessage(BsmMessage &msg, routeable_message &routeableMsg);
+	void SubscribeKafkaTopics();
+	bool getEncodedtsm3(tsm3EncodedMessage *tsm3EncodedMsg,  Json::Value metadata, Json::Value payload_json);
 
 private:
 	std::string _receiveTopic;
 	std::string _transmitMobilityOperationTopic;
+	std::string _subscribeToSchedulingPlanTopic = "";
 	std::string _transmitMobilityPathTopic;
+	std::string _transmitBSMTopic;
 	std::string _kafkaBrokerIp;
 	std::string _kafkaBrokerPort;
 	RdKafka::Conf *kafka_conf;
+	RdKafka::Conf *kafka_conf_consumer;
  	RdKafka::Producer *kafka_producer;
-	RdKafka::Consumer *kafka_consumer;
+	RdKafka::KafkaConsumer *kafka_consumer;
+	RdKafka::Topic *_topic;
 	std::vector<std::string> _strategies;
+	tmx::messages::tsm3Message *_tsm3Message{NULL};
+	std::mutex data_lock;
+		
+	/***
+	 * Configurable indicator to run consumer and consume messages from kafka topics
+	 * run the consumer if it equals = 1; otherwise = 0
+	**/
+	int _run_kafka_consumer = 0; 
+	std::string _intersectionType = "NA";
 };
 std::mutex _cfgLock;
 
