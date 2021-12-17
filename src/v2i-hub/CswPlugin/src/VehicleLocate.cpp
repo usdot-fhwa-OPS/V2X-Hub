7a16338b98297b816c91185c116df8d6e64b2b1f
Saikrishna Bairamoni <84093461+SaikrishnaBairamoni@users.noreply.github.com>
2021-12-17 15:25:00 -0500

    Revert "merge Cswplugin sonar issues branch to develop" (#293)

--- src/v2i-hub/CswPlugin/src/VehicleLocate.cpp
+++ src/v2i-hub/CswPlugin/src/VehicleLocate.cpp
@@ -76,11 +76,9 @@ int VehicleLocate::FindRegion(TiDataFrame *frame, WGS84Point point, uint16_t hea
 		WGS84Point* points = GetPointArray(nodes, nodesLength, nodesAnchor);
 
 		if (IsInPointList(points, nodesLength, point, heading, laneWidth/100.0))
-			free(points);
 			return i + 1;
-		
+
 		free(points);
-		
 	}
 
 	return -1;
