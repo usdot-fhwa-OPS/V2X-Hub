217f742e0c4106b350899a6e15071dd654909d07
dan-du-car <dan.du@leidos.com>
2022-03-10 15:45:34 -0800

    update strategy label

--- src/v2i-hub/CARMACloudPlugin/src/CARMACloudPlugin.h
+++ src/v2i-hub/CARMACloudPlugin/src/CARMACloudPlugin.h
@@ -159,7 +159,7 @@ private:
 	//TCM repeatedly broadcast time out in unit of second
 	uint16_t _TCMRepeatedlyBroadcastTimeOut = 0;
 	std::string _TCMNOAcknowledgementDescription = "";
-	string _TCMAcknowledgementStrategy = "carma3/geofence_acknowledgement";
+	const string _TCMAcknowledgementStrategy = "carma3/geofence_acknowledgement";
 
 };
 std::mutex _cfgLock;
