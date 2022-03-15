c026fa6f4167de42b6089108a8b983ca464b60cf
dan-du-car <62157949+dan-du-car@users.noreply.github.com>
2022-03-15 14:32:55 -0700

    Carma freight wz update (#342)
    * init
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update no ACK comments
    
    * update description
    
    * remove print
    
    * remove comment out code
    
    * update J2735 files to include refwidth, maxplatoonsize minplatoonhdwy
    
    * address code smell
    
    * update
    
    * update with exit point
    
    * update NO ack message
    
    * update strategy label
    
    * update strategy label
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- src/v2i-hub/CARMACloudPlugin/src/CARMACloudPlugin.cpp
+++ src/v2i-hub/CARMACloudPlugin/src/CARMACloudPlugin.cpp
@@ -17,22 +17,22 @@ namespace CARMACloudPlugin {
  */
 CARMACloudPlugin::CARMACloudPlugin(string name) :PluginClient(name) {
 
-		// xml parser setup 
-		//this->Cli.SetProxy(QString::fromLocal8Bit("http"),QString::fromLocal8Bit("127.0.0.1"),22222,QString::fromLocal8Bit("/v1/carmacloud"),0);
-        std::lock_guard<mutex> lock(_cfgLock);
-		GetConfigValue<string>("WebServiceIP",webip);
-		GetConfigValue<uint16_t>("WebServicePort",webport);
-		GetConfigValue<uint16_t>("fetchTime",fetchtime);		
-		AddMessageFilter < tsm4Message > (this, &CARMACloudPlugin::HandleCARMARequest);
+	UpdateConfigSettings();
+	std::lock_guard<mutex> lock(_cfgLock);
+	AddMessageFilter < tsm4Message > (this, &CARMACloudPlugin::HandleCARMARequest);
+	AddMessageFilter < tsm3Message > (this, &CARMACloudPlugin::HandleMobilityOperationMessage);
 
-		// Subscribe to all messages specified by the filters above.
-		SubscribeToMessages();
-		std::thread webthread(&CARMACloudPlugin::StartWebService,this);
-		webthread.detach(); // wait for the thread to finish 
-		url ="http://127.0.0.1:33333"; // 33333 is the port that will send from v2xhub to carma cloud ## initally was 23665
-		base_hb = "/carmacloud/v2xhub";
-		base_req = "/carmacloud/tcmreq";
-		method = "POST";
+	// Subscribe to all messages specified by the filters above.
+	SubscribeToMessages();
+	std::thread webthread(&CARMACloudPlugin::StartWebService,this);
+	webthread.detach(); // wait for the thread to finish 
+	url ="http://127.0.0.1:33333"; // 33333 is the port that will send from v2xhub to carma cloud ## initally was 23665
+	base_hb = "/carmacloud/v2xhub";
+	base_req = "/carmacloud/tcmreq";
+	method = "POST";
+	_not_ACK_TCMs = std::make_shared<multimap<string, tsm5EncodedMessage>>();
+	std::thread Broadcast_t(&CARMACloudPlugin::Broadcast_TCMs, this);
+	Broadcast_t.detach();
 
 }
 
@@ -41,64 +41,108 @@ void CARMACloudPlugin::HandleCARMARequest(tsm4Message &msg, routeable_message &r
 {
 	auto carmaRequest = msg.get_j2735_data();
 
-	// create an XML template for the request
-	//if(carmaRequest->body.present == TrafficControlRequest_PR_tcrV01) // taking this out since some message arent enabling this present variable. 
-	// {
-
-        // convert reqid bytes to hex string.
-        size_t hexlen = 2; //size of each hex representation with a leading 0
-        char reqid[carmaRequest->body.choice.tcrV01.reqid.size * hexlen + 1];
-        for (int i = 0; i < carmaRequest->body.choice.tcrV01.reqid.size; i++)
-        {
-            sprintf(reqid+(i*hexlen), "%.2X", carmaRequest->body.choice.tcrV01.reqid.buf[i]);
-        }
-
-		printf("%s\n",reqid);
-
-		long int reqseq = carmaRequest->body.choice.tcrV01.reqseq;
-		long int scale = carmaRequest->body.choice.tcrV01.scale;
-		
-
-		int totBounds =  carmaRequest->body.choice.tcrV01.bounds.list.count;
-		int cnt=0;
-		char bounds_str[5000];
-	       	strcpy(bounds_str,"");	
-		
-		//  get current time 
-		std::time_t tm = std::time(0)/60-fetchtime*24*60; //  T minus 24 hours in  min  
-
-		while(cnt<totBounds)
-		{
-
-			uint32_t oldest=tm;
-		//	GetInt32((unsigned char*)carmaRequest->body.choice.tcrV01.bounds.list.array[cnt]->oldest.buf,&oldest);
-			// = (int*)  carmaRequest->body.choice.tcrV01.bounds.list.array[cnt]->oldest.buf;
-			long lat = carmaRequest->body.choice.tcrV01.bounds.list.array[cnt]->reflat; 
-			long longg = carmaRequest->body.choice.tcrV01.bounds.list.array[cnt]->reflon;
-
-		
-			long dtx0 = carmaRequest->body.choice.tcrV01.bounds.list.array[cnt]->offsets.list.array[0]->deltax;
-			long dty0 = carmaRequest->body.choice.tcrV01.bounds.list.array[cnt]->offsets.list.array[0]->deltay;
-			long dtx1 = carmaRequest->body.choice.tcrV01.bounds.list.array[cnt]->offsets.list.array[1]->deltax;
-			long dty1 = carmaRequest->body.choice.tcrV01.bounds.list.array[cnt]->offsets.list.array[1]->deltay;
-			long dtx2 = carmaRequest->body.choice.tcrV01.bounds.list.array[cnt]->offsets.list.array[2]->deltax;
-			long dty2 = carmaRequest->body.choice.tcrV01.bounds.list.array[cnt]->offsets.list.array[2]->deltay;
-
-			sprintf(bounds_str+strlen(bounds_str),"<bounds><oldest>%ld</oldest><reflon>%ld</reflon><reflat>%ld</reflat><offsets><deltax>%ld</deltax><deltay>%ld</deltay></offsets><offsets><deltax>%ld</deltax><deltay>%ld</deltay></offsets><offsets><deltax>%ld</deltax><deltay>%ld</deltay></offsets></bounds>",oldest,longg,lat,dtx0,dty0,dtx1,dty1,dtx2,dty2);
-
-			cnt++;
 
 
-		}
+	// convert reqid bytes to hex string.
+	size_t hexlen = 2; //size of each hex representation with a leading 0
+	char reqid[carmaRequest->body.choice.tcrV01.reqid.size * hexlen + 1];
+	for (int i = 0; i < carmaRequest->body.choice.tcrV01.reqid.size; i++)
+	{
+		sprintf(reqid+(i*hexlen), "%.2X", carmaRequest->body.choice.tcrV01.reqid.buf[i]);
+	}
 
-		char xml_str[10000]; 
-        sprintf(xml_str,"<?xml version=\"1.0\" encoding=\"UTF-8\"?><TrafficControlRequest><reqid>%s</reqid><reqseq>%ld</reqseq><scale>%ld</scale>%s</TrafficControlRequest>",reqid, reqseq,scale,bounds_str);
+	printf("%s\n",reqid);
 
-	cout<<"Sent TCR: "<<xml_str<<endl;
+	long int reqseq = carmaRequest->body.choice.tcrV01.reqseq;
+	long int scale = carmaRequest->body.choice.tcrV01.scale;
+	
+
+	int totBounds =  carmaRequest->body.choice.tcrV01.bounds.list.count;
+	int cnt=0;
+	char bounds_str[5000];
+		strcpy(bounds_str,"");	
+	
+	//  get current time 
+	std::time_t tm = std::time(0)/60-fetchtime*24*60; //  T minus 24 hours in  min  
+
+	while(cnt<totBounds)
+	{
+
+		uint32_t oldest=tm;
+		long lat = carmaRequest->body.choice.tcrV01.bounds.list.array[cnt]->reflat; 
+		long longg = carmaRequest->body.choice.tcrV01.bounds.list.array[cnt]->reflon;
+
+	
+		long dtx0 = carmaRequest->body.choice.tcrV01.bounds.list.array[cnt]->offsets.list.array[0]->deltax;
+		long dty0 = carmaRequest->body.choice.tcrV01.bounds.list.array[cnt]->offsets.list.array[0]->deltay;
+		long dtx1 = carmaRequest->body.choice.tcrV01.bounds.list.array[cnt]->offsets.list.array[1]->deltax;
+		long dty1 = carmaRequest->body.choice.tcrV01.bounds.list.array[cnt]->offsets.list.array[1]->deltay;
+		long dtx2 = carmaRequest->body.choice.tcrV01.bounds.list.array[cnt]->offsets.list.array[2]->deltax;
+		long dty2 = carmaRequest->body.choice.tcrV01.bounds.list.array[cnt]->offsets.list.array[2]->deltay;
+
+		sprintf(bounds_str+strlen(bounds_str),"<bounds><oldest>%ld</oldest><reflon>%ld</reflon><reflat>%ld</reflat><offsets><deltax>%ld</deltax><deltay>%ld</deltay></offsets><offsets><deltax>%ld</deltax><deltay>%ld</deltay></offsets><offsets><deltax>%ld</deltax><deltay>%ld</deltay></offsets></bounds>",oldest,longg,lat,dtx0,dty0,dtx1,dty1,dtx2,dty2);
+
+		cnt++;
+
+
+	}
+
+	char xml_str[10000]; 
+	sprintf(xml_str,"<?xml version=\"1.0\" encoding=\"UTF-8\"?><TrafficControlRequest><reqid>%s</reqid><reqseq>%ld</reqseq><scale>%ld</scale>%s</TrafficControlRequest>",reqid, reqseq,scale,bounds_str);
+
+	PLOG(logINFO) << "Sent TCR: "<< xml_str<<endl;
 	CloudSend(xml_str,url, base_req, method);
-	//}
 
+}
 
+void CARMACloudPlugin::HandleMobilityOperationMessage(tsm3Message &msg, routeable_message &routeableMsg){
+	std::vector<string> strategies_v;
+	ConvertString2Vector(strategies_v, _strategies);
+
+	//Process incoming MobilityOperation message
+	auto mobilityOperationMsg = msg.get_j2735_data();
+	
+	//process MobilityOperation strategy 
+	stringstream ss;
+	ss << mobilityOperationMsg->body.strategy.buf;
+	string mo_strategy = ss.str();
+	std::transform(mo_strategy.begin(), mo_strategy.end(), mo_strategy.begin(), ::tolower );
+
+	if( std::find( strategies_v.begin(), strategies_v.end(), mo_strategy ) != strategies_v.end() && mo_strategy.find(_TCMAcknowledgementStrategy) != std::string::npos)
+	{		
+		//Process MobilityOperation strategy params
+		ss.str("");
+		ss << mobilityOperationMsg->body.operationParams.buf;
+		string strategy_params_str = ss.str();	
+		std::vector<string> strategy_params_v;
+		ConvertString2Vector(strategy_params_v, strategy_params_str);	
+
+		string even_log_description = GetValueFromStrategyParamsByKey(strategy_params_v, "reason");
+		string acknnowledgement_str = GetValueFromStrategyParamsByKey(strategy_params_v, "acknowledgement");
+		string traffic_control_id = GetValueFromStrategyParamsByKey(strategy_params_v, "traffic_control_id");
+		boost::trim(traffic_control_id);
+		std::transform(traffic_control_id.begin(), traffic_control_id.end(), traffic_control_id.begin(), ::tolower );	
+		ss.str("");
+		ss << mobilityOperationMsg->header.hostStaticId.buf;
+		string CMV_id = ss.str();
+
+		std::lock_guard<mutex> lock(_not_ACK_TCMs_mutex);
+		//The traffic control id should match with the TCM id per CMV (CARMA vehicle).	
+		if(_not_ACK_TCMs->erase(traffic_control_id) <= 0)
+		{
+			PLOG(logERROR) << "Acknowledgement received, but traffic_control_id =" << traffic_control_id << " Not Found in TCM map." << std::endl;
+		}
+		
+		//Create an event log object for both positive and negative ACK (ackownledgement), and broadcast the event log
+		tmx::messages::TmxEventLogMessage event_log_msg;
+
+		//acknnowledgement: Flag to indicate whether the received geofence was processed successfully by the CAV	
+		std::transform(acknnowledgement_str.begin(), acknnowledgement_str.end(), acknnowledgement_str.begin(), ::tolower );	
+		acknnowledgement_str.find("true") != std::string::npos ? event_log_msg.set_level(IvpLogLevel::IvpLogLevel_info) : event_log_msg.set_level(IvpLogLevel::IvpLogLevel_warn);
+		event_log_msg.set_description(mo_strategy + ": Traffic control id = " + traffic_control_id + ( CMV_id.length() <= 0 ? "":", CMV Id = " + CMV_id )+ ", reason = " + even_log_description);
+		PLOG(logDEBUG) << "event_log_msg " << event_log_msg << std::endl;
+		this->BroadcastMessage<tmx::messages::TmxEventLogMessage>(event_log_msg);	
+	}
 }
 
 CARMACloudPlugin::~CARMACloudPlugin() {
@@ -143,12 +187,10 @@ void CARMACloudPlugin::CARMAResponseHandler(QHttpEngine::Socket *socket)
 	tcm=updateTags(tcm,"TrafficControlParams","params");
 	tcm=updateTags(tcm,"TrafficControlGeometry","geometry");
 	tcm=updateTags(tcm,"TrafficControlPackage","package");
-
 	
 	tsm5Message tsm5message;
 	tsm5EncodedMessage tsm5ENC;
 	tmx::message_container_type container;
-	std::unique_ptr<tsm5EncodedMessage> msg;
 
 
 	std::stringstream ss;
@@ -158,24 +200,88 @@ void CARMACloudPlugin::CARMAResponseHandler(QHttpEngine::Socket *socket)
 	tsm5message.set_contents(container.get_storage().get_tree());
 	tsm5ENC.encode_j2735_message(tsm5message);
 
-	msg.reset();
-	msg.reset(dynamic_cast<tsm5EncodedMessage*>(factory.NewMessage(api::MSGSUBTYPE_TESTMESSAGE05_STRING)));
-
-	string enc = tsm5ENC.get_encoding();
-	msg->refresh_timestamp();
-	msg->set_payload(tsm5ENC.get_payload_str());
-	msg->set_encoding(enc);
-	msg->set_flags(IvpMsgFlags_RouteDSRC);
-	msg->addDsrcMetadata(172, 0x8003);
-	msg->refresh_timestamp();
-
-	routeable_message *rMsg = dynamic_cast<routeable_message *>(msg.get());
-	BroadcastMessage(*rMsg);
-
-	PLOG(logERROR) << " CARMACloud Plugin :: Broadcast tsm5:: " << tsm5ENC.get_payload_str();
+	//Get TCM id
+	Id64b_t tcmv01_req_id = tsm5message.get_j2735_data()->body.choice.tcmV01.reqid;
+	
+	ss.str(""); 
+    for(size_t i=0; i < tcmv01_req_id.size; i++)
+    {
+		ss << std::setfill('0') << std::setw(2) << std::hex << (unsigned) tcmv01_req_id.buf[i];
+    }
+	string tcmv01_req_id_hex = ss.str();	
+	
+	std::transform(tcmv01_req_id_hex.begin(), tcmv01_req_id_hex.end(), tcmv01_req_id_hex.begin(), ::tolower );	
+	if(tcmv01_req_id_hex.length() > 0)
+	{
+		std::lock_guard<mutex> lock(_not_ACK_TCMs_mutex);
+		_not_ACK_TCMs->insert({tcmv01_req_id_hex, tsm5ENC});
+	}	
 }
 
+void CARMACloudPlugin::Broadcast_TCMs()
+{ 	
+	std::time_t start_time = 0, cur_time = 0;
+	bool is_started_broadcasting = false;
+	while(true)
+	{			
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		if(_plugin->state == IvpPluginState_error)
+		{
+			break;
+		}
+		if(_not_ACK_TCMs->size() > 0)
+		{
+			std::lock_guard<mutex> lock(_not_ACK_TCMs_mutex);
+			for( auto itr = _not_ACK_TCMs->begin(); itr!=_not_ACK_TCMs->end(); ++itr )
+			{
+				string tcmv01_req_id_hex = itr->first;
+				if(!is_started_broadcasting)
+				{
+					start_time = (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch())).count();
+				}else{
+					cur_time = (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch())).count();
+				}
+				is_started_broadcasting = true;
+				if((cur_time - start_time) > _TCMRepeatedlyBroadcastTimeOut)
+				{
+					_not_ACK_TCMs->erase(tcmv01_req_id_hex);
+					start_time = 0;
+					cur_time = 0;	
+					is_started_broadcasting = false;
 
+					//Create an event log object for both NO ACK (ackownledgement), and broadcast the event log
+					tmx::messages::TmxEventLogMessage event_log_msg;
+					event_log_msg.set_level(IvpLogLevel::IvpLogLevel_warn);
+					event_log_msg.set_description(_TCMAcknowledgementStrategy + ": " + _TCMNOAcknowledgementDescription + " Traffic control id = " + tcmv01_req_id_hex);
+					PLOG(logDEBUG) << "event_log_msg " << event_log_msg << std::endl;
+					this->BroadcastMessage<tmx::messages::TmxEventLogMessage>(event_log_msg);	
+					break;
+				}
+				std::unique_ptr<tsm5EncodedMessage> msg;
+				msg.reset();
+				msg.reset(dynamic_cast<tsm5EncodedMessage*>(factory.NewMessage(api::MSGSUBTYPE_TESTMESSAGE05_STRING)));
+				tsm5EncodedMessage tsm5ENC = itr->second;
+				string enc = tsm5ENC.get_encoding();
+				msg->refresh_timestamp();
+				msg->set_payload(tsm5ENC.get_payload_str());
+				msg->set_encoding(enc);
+				msg->set_flags(IvpMsgFlags_RouteDSRC);
+				msg->addDsrcMetadata(172, 0x8003);
+				msg->refresh_timestamp();
+
+				routeable_message *rMsg = dynamic_cast<routeable_message *>(msg.get());
+				BroadcastMessage(*rMsg);		
+				PLOG(logINFO) << " CARMACloud Plugin :: Broadcast tsm5:: " << tsm5ENC.get_payload_str();
+			}
+		}
+		else
+		{
+			start_time = 0;
+			cur_time = 0;	
+			is_started_broadcasting = false;
+		}
+	}
+}
 
 int CARMACloudPlugin::StartWebService()
 {
@@ -216,13 +322,14 @@ int CARMACloudPlugin::StartWebService()
 
 
 void CARMACloudPlugin::UpdateConfigSettings() {
-
-	GetConfigValue<uint64_t>("Frequency", _frequency);
-	
     std::lock_guard<mutex> lock(_cfgLock);
+	GetConfigValue<uint64_t>("Frequency", _frequency);	
 	GetConfigValue<string>("WebServiceIP",webip);
 	GetConfigValue<uint16_t>("WebServicePort",webport);
 	GetConfigValue<uint16_t>("fetchTime",fetchtime);
+	GetConfigValue<string>("MobilityOperationStrategies", _strategies);	
+	GetConfigValue<uint16_t>("TCMRepeatedlyBroadcastTimeOut",_TCMRepeatedlyBroadcastTimeOut);	
+	GetConfigValue<string>("TCMNOAcknowledgementDescription", _TCMNOAcknowledgementDescription);
 	
 }
 
@@ -266,7 +373,55 @@ int CARMACloudPlugin::CloudSend(string msg,string url, string base, string metho
   return 0;
 }
 
+void CARMACloudPlugin::ConvertString2Vector(std::vector<string> &sub_str_v, const string &str) const{
+	stringstream ss;
+	ss << str;
+	while (ss.good())
+	{
+		std::string substring;
+		getline(ss, substring, ',');		
+		std::transform(substring.begin(), substring.end(),substring.begin(), ::tolower );
+		sub_str_v.push_back( substring );
+	}	
+}
 
+string CARMACloudPlugin::GetValueFromStrategyParamsByKey(const std::vector<string> & strategy_params_v, const string& key) const
+{	
+	string value = "";
+	for(auto itr = strategy_params_v.begin();  itr != strategy_params_v.end(); itr++)
+	{
+		std::pair<string, string> key_value_pair;
+		ConvertString2Pair(key_value_pair, *itr);
+		if (key_value_pair.first.find(key) != std::string::npos)
+		{
+			value = key_value_pair.second;
+			return value;
+		}		
+	}
+	return value;		
+}
+
+void CARMACloudPlugin::ConvertString2Pair(std::pair<string,string> &str_pair, const string &str) const
+{
+	stringstream ss;
+	ss << str;
+	size_t count = 0;
+	string key, value;
+	while (ss.good())
+	{
+		std::string substring;
+		getline(ss, substring, ':');
+		if(count == 0)
+		{
+			std::transform(substring.begin(), substring.end(),substring.begin(), ::tolower );
+			key = substring;
+			count += 1;
+		}else{
+			value = substring;
+		}		
+	}	
+	str_pair = std::make_pair(key, value);
+}
 
 int CARMACloudPlugin::Main() {
 
--- src/v2i-hub/CARMACloudPlugin/src/CARMACloudPlugin.h
+++ src/v2i-hub/CARMACloudPlugin/src/CARMACloudPlugin.h
@@ -70,7 +70,7 @@
 
 
 #include <curl/curl.h>
-
+#include <algorithm>
 
 
 
@@ -108,6 +108,7 @@ protected:
 	string updateTags(string s,string t, string t1);
 
 	void HandleCARMARequest(tsm4Message &msg, routeable_message &routeableMsg);
+	void HandleMobilityOperationMessage(tsm3Message &msg, routeable_message &routeableMsg);
 	void GetInt32(unsigned char *buf, int32_t *value)
 	{
 		*value = (int32_t)((buf[0] << 24) + (buf[1] << 16) + (buf[2] << 8) + buf[3]);
@@ -118,7 +119,31 @@ protected:
 		*value = (uint64_t)((buf[0] << 56) + (buf[1] << 48) + (buf[2] << 40) + (buf[3] << 32) + (buf[4] << 24) + (buf[5] << 16) + (buf[6] << 8) + buf[7]);
 	}
 
+	/***
+	 * @brief: Insert the comma separated string into the  list
+	 * @param: std::vector of substrings
+	 * @param: comma separated string 
+	 * **/
+	void ConvertString2Vector(std::vector<string> &sub_str_v, const string &str) const;
 
+	/***
+	 * @brief: Insert the colon separated string into the pair
+	 * @param: std::pair<string, string> of string
+	 * @param: colon separated string 
+	 * **/
+	void ConvertString2Pair(std::pair<string,string> &str_pair, const string &str) const;
+
+	/***
+	 * @brief: Retrieve the value string from the strategy params based on the input key string
+	 * @param: std::vector of comma separated string in stragety_params
+	 * @param: key string to identify the value from the key-value pair separated by colon. Input key should be all lower case
+	 * **/
+	string GetValueFromStrategyParamsByKey(const std::vector<string> & stragety_params_v, const string &key) const;
+	/***
+	 * @brief: Loop through the received TCMs and broadcast them for the configured duration.
+	 * If it timed out, it would remove the TCMs from the list, and stop broadcasting them.
+	 * ***/
+	void Broadcast_TCMs();
 
 private:
 
@@ -127,7 +152,18 @@ private:
 	J2735MessageFactory factory;
 	uint64_t _frequency = 0;
 	string url,base_hb, base_req, method; 
+	
+	//Comma separated string for list of strategies from MobilityOperation messages
+	std::string _strategies;
 
+	//Used to lock the shared TCMs resource
+	std::mutex _not_ACK_TCMs_mutex;
+	//An associated array to keep track of TCMs that are not acknowledged
+	std::shared_ptr<std::multimap<string, tsm5EncodedMessage>> _not_ACK_TCMs;
+	//TCM repeatedly broadcast time out in unit of second
+	uint16_t _TCMRepeatedlyBroadcastTimeOut = 0;
+	std::string _TCMNOAcknowledgementDescription = "";
+	const string _TCMAcknowledgementStrategy = "carma3/geofence_acknowledgement";
 
 };
 std::mutex _cfgLock;
