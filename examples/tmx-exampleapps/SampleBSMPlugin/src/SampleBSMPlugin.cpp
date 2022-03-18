74c03699ccb5750d7392f1f5dad42ffdcad2c0e0
dan-du-car <62157949+dan-du-car@users.noreply.github.com>
2022-03-18 10:36:14 -0700

    Sample BSM plugin (#343)
    * init
    
    * update
    
    * remove not used code
    
    * update
    
    * update
    
    * update

---
+++ examples/tmx-exampleapps/SampleBSMPlugin/src/SampleBSMPlugin.cpp
@@ -0,0 +1,144 @@
+#include "SampleBSMPlugin.h"
+using namespace tmx;
+using namespace tmx::utils;
+using namespace tmx::messages;
+
+namespace SampleBSMPluginNS
+{
+    SampleBSMPlugin::SampleBSMPlugin(std::string name) : PluginClient(name)
+    {
+        AddMessageFilter<BsmMessage>(this, &SampleBSMPlugin::HandleBasicSafetyMessage);
+        SubscribeToMessages();
+
+        //Create and broadcast new BSM
+        std::thread t1(&SampleBSMPlugin::EncodeBroadcastBSM, this);
+        t1.join();
+    }
+
+    void SampleBSMPlugin::HandleBasicSafetyMessage(BsmMessage &msg, routeable_message &routeableMsg)
+    {
+        //Getting BSM from MessageReceiver plugin
+        auto bsm = msg.get_j2735_data();  
+             
+        BsmEncodedMessage bsmEncodeMessage;
+      
+        BsmMessage* bsmMessage = new BsmMessage(bsm);        
+        MessageFrameMessage frame_msg(bsmMessage->get_j2735_data());
+        bsmEncodeMessage.set_data(TmxJ2735EncodedMessage<BasicSafetyMessage>::encode_j2735_message<codec::uper<MessageFrameMessage>>(frame_msg));   
+        bsmEncodeMessage.set_flags(IvpMsgFlags_RouteDSRC);
+        bsmEncodeMessage.addDsrcMetadata(172, 0x20);
+        bsmEncodeMessage.refresh_timestamp();
+
+        routeable_message *rMsg = dynamic_cast<routeable_message *>(&bsmEncodeMessage);
+        BroadcastMessage(*rMsg);
+        PLOG(logERROR) << " SampleBSMPlugind :: Broadcast BSM:: " << bsmEncodeMessage.get_payload_str();
+        free(frame_msg.get_j2735_data().get()); 
+        delete bsmMessage;
+        
+    }
+
+    void SampleBSMPlugin::EncodeBroadcastBSM()
+    {
+        while (true)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(2000)); 
+
+            if(IsPluginState(IvpPluginState_error) )
+            {
+                break;
+            }
+
+            if (IsPluginState(IvpPluginState_disconnected) || !IsPluginState(IvpPluginState_registered))
+            {
+                continue;
+            }
+
+            //Create new BSM                 
+            BsmEncodedMessage bsmEncoded;
+            EncodeBSM( bsmEncoded );
+
+            //Broadcast BSM
+            bsmEncoded.set_flags(IvpMsgFlags_RouteDSRC);
+            bsmEncoded.addDsrcMetadata(172, 0x20);
+            bsmEncoded.refresh_timestamp();
+            routeable_message *rMsg = dynamic_cast<routeable_message *>(&bsmEncoded);
+            BroadcastMessage(*rMsg);
+            PLOG(logERROR) << " SampleBSMPlugin :: Broadcast CREATED BSM:: " << bsmEncoded.get_payload_str();
+        }       
+    }
+
+    void SampleBSMPlugin::EncodeBSM(BsmEncodedMessage& bsmEncoded) const
+    {   
+        BasicSafetyMessage_t* bsm = (BasicSafetyMessage_t*) calloc(1, sizeof(BasicSafetyMessage_t) );  
+        char* my_str = (char *) "sender_id";
+        uint8_t* my_bytes = reinterpret_cast<uint8_t *>(my_str);
+        bsm->coreData.msgCnt = 1;
+        uint8_t  my_bytes_id[4] = {(uint8_t)1, (uint8_t)12, (uint8_t)12, (uint8_t)10};
+        bsm->coreData.id.buf = my_bytes_id;
+        bsm->coreData.id.size = sizeof(my_bytes_id);
+        bsm->coreData.secMark = 1023;
+        bsm->coreData.lat = 38954961;
+        bsm->coreData.Long = -77149303;
+        bsm->coreData.elev = 72;
+        bsm->coreData.speed = 100;
+        bsm->coreData.heading = 12;
+        bsm->coreData.angle = 10;
+        bsm->coreData.transmission = 0;          
+
+        //position accuracy
+        bsm->coreData.accuracy.orientation= 100;
+        bsm->coreData.accuracy.semiMajor = 200;
+        bsm->coreData.accuracy.semiMinor = 200;
+
+        //Acceleration set
+        bsm->coreData.accelSet.lat = 100;
+        bsm->coreData.accelSet.Long = 300;
+        bsm->coreData.accelSet.vert = 100;
+        bsm->coreData.accelSet.yaw = 0;
+
+        //populate brakes
+        bsm->coreData.brakes.abs = 1; 
+        bsm->coreData.brakes.scs = 1; 
+        bsm->coreData.brakes.traction = 1; 
+        bsm->coreData.brakes.brakeBoost = 1; 
+        bsm->coreData.brakes.auxBrakes = 1; 
+        uint8_t  my_bytes_brakes[1] = {8};
+        bsm->coreData.brakes.wheelBrakes.buf = my_bytes_brakes; 
+        bsm->coreData.brakes.wheelBrakes.size = sizeof(my_bytes_brakes); 
+        bsm->coreData.brakes.wheelBrakes.bits_unused = 3; 
+
+        //vehicle size
+        bsm->coreData.size.length = 500;
+        bsm->coreData.size.width = 300;
+
+        std::cout << "createAndSendBSM 7" << std::endl;         
+        BsmMessage*  bsmMessage = new BsmMessage(bsm);
+        MessageFrameMessage messageFrame( bsmMessage->get_j2735_data() );
+        std::cout << "createAndSendBSM 7.1" << std::endl;
+        bsmEncoded.set_data( TmxJ2735EncodedMessage<BasicSafetyMessage>::encode_j2735_message<codec::uper<MessageFrameMessage>>(messageFrame) );
+        std::cout << "createAndSendBSM 7.2" << std::endl;
+        free(bsm);
+        free(messageFrame.get_j2735_data().get());     
+    }
+
+    SampleBSMPlugin::~SampleBSMPlugin() {};
+
+    int SampleBSMPlugin::Main()
+    {
+        FILE_LOG(logINFO) << "Starting plugin";
+        while (_plugin->state != IvpPluginState_error)
+        {
+            if (IsPluginState(IvpPluginState_registered))
+            {
+                std::this_thread::sleep_for(std::chrono::milliseconds(5000));                
+            }
+        }
+        return (EXIT_SUCCESS);
+    }
+
+}
+
+int main(int argc, char *argv[])
+{
+    return run_plugin<SampleBSMPluginNS::SampleBSMPlugin>("SampleBSMPlugin", argc, argv);
+}
