09f935916a1414ad1830a42665106415aeec855d
Rob Baily <rob.baily@leidos.com>
2022-12-14 10:30:52 -0500

    First cut at updated CI which does not use Docker image for Sonar

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,11 +3,6 @@
 # exit on errors
 set -e
 
-DEPLOY_IMAGE=0
-if [ "$1" = "deploy" ]; then
-    DEPLOY_IMAGE=1
-fi
-
 apt-get update
 
 # install all things needed for deployment, always done
@@ -33,31 +28,3 @@ apt-get install -y \
     libev-dev \
     libuv1-dev \
     libcpprest-dev
-
-if [ $DEPLOY_IMAGE -eq 0 ]; then
-# install other things needed for development
-apt-get install -y \
-    sudo \
-    libusb-dev \
-    libusb-1.0-0-dev \
-    libftdi-dev \
-    swig \
-    liboctave-dev \
-    gpsd \
-    portaudio19-dev \
-    libsndfile1-dev \
-    libglib2.0-dev \
-    libglibmm-2.4-dev \
-    libpcre3-dev \
-    libsigc++-2.0-dev \
-    libxml++2.6-dev \
-    libxml2-dev \
-    liblzma-dev \
-    dpkg-dev \
-    vim \
-    qtbase5-dev-tools \
-    curl \
-    libqhttpengine-dev
-fi
-
-
