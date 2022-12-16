6b8decafad21201f325d0da07c3dcc2301d2ef47
Cody Garver <dylan.garver@leidos.com>
2022-12-16 13:09:45 -0500

    Fix setup.sh

--- container/setup.sh
+++ container/setup.sh
@@ -5,6 +5,5 @@ cp /home/V2X-Hub/src/tmx/TmxCore/tmxcore.service /lib/systemd/system/
 cp /home/V2X-Hub/src/tmx/TmxCore/tmxcore.service /usr/sbin/
 chmod +x /home/V2X-Hub/container/service.sh /home/V2X-Hub/container/wait-for-it.sh
 mkdir -p /var/www/plugins/MAP /var/www/plugins/.ssl
-chown plugin:www-data /var/www/plugins/.ssl
-openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout /var/www/plugins/.ssl/tmxcmd.key -out tmxcmd.crt -subj "/CN= <127.0.0.1> " -days 3650
-chown plugin:www-data /var/www/plugins/.ssl/*
+openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout /var/www/plugins/.ssl/tmxcmd.key -out /var/www/plugins/.ssl/tmxcmd.crt -subj "/CN= <127.0.0.1> " -days 3650
+chown -R plugin:www-data /var/www/plugins/.ssl
