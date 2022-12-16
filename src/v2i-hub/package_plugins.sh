80a60192fe5812446227c98a0c42fcab9ecb3749
Cody Garver <dylan.garver@leidos.com>
2022-12-16 11:25:43 -0500

    Array for packaging plugins

--- src/v2i-hub/package_plugins.sh
+++ src/v2i-hub/package_plugins.sh
@@ -1,40 +1,28 @@
-#!/bin/sh
-
-# exit on errors
+#!/bin/bash
 set -e
 
+PLUGINS=(
+    CARMACloudPlugin
+    CARMAStreetsPlugin
+    CommandPlugin
+    CswPlugin
+    DmsPlugin
+    DsrcImmediateForwardPlugin
+    LocationPlugin
+    MapPlugin
+    MessageLoggerPlugin
+    MessageReceiverPlugin
+    ODELoggerPlugin
+    PedestrianPlugin
+    PortDrayagePlugin
+    PreemptionPlugin
+    RtcmPlugin
+    SpatPlugin
+    TimPlugin
+)
+
 # package up all plugins
-ln -s ../bin CommandPlugin/bin
-zip CommandPlugin.zip CommandPlugin/bin/CommandPlugin CommandPlugin/manifest.json
-ln -s ../bin CswPlugin/bin
-zip CswPlugin.zip CswPlugin/bin/CswPlugin CswPlugin/manifest.json
-ln -s ../bin DmsPlugin/bin
-zip DmsPlugin.zip DmsPlugin/bin/DmsPlugin DmsPlugin/manifest.json
-ln -s ../bin DsrcImmediateForwardPlugin/bin
-zip DsrcImmediateForwardPlugin.zip DsrcImmediateForwardPlugin/bin/DsrcImmediateForwardPlugin DsrcImmediateForwardPlugin/manifest.json
-ln -s ../bin LocationPlugin/bin
-zip LocationPlugin.zip LocationPlugin/bin/LocationPlugin LocationPlugin/manifest.json
-ln -s ../bin MapPlugin/bin
-zip MapPlugin.zip MapPlugin/bin/MapPlugin MapPlugin/manifest.json
-ln -s ../bin MessageReceiverPlugin/bin
-zip MessageReceiverPlugin.zip MessageReceiverPlugin/bin/MessageReceiverPlugin MessageReceiverPlugin/manifest.json
-ln -s ../bin RtcmPlugin/bin
-zip RtcmPlugin.zip RtcmPlugin/bin/RtcmPlugin RtcmPlugin/manifest.json
-ln -s ../bin SpatPlugin/bin
-zip SpatPlugin.zip SpatPlugin/bin/SpatPlugin SpatPlugin/manifest.json
-ln -s ../bin PreemptionPlugin/bin
-zip PreemptionPlugin.zip PreemptionPlugin/bin/PreemptionPlugin PreemptionPlugin/manifest.json
-ln -s ../bin MessageLoggerPlugin/bin
-zip MessageLoggerPlugin.zip MessageLoggerPlugin/bin/MessageLoggerPlugin MessageLoggerPlugin/manifest.json
-ln -s ../bin PedestrianPlugin/bin
-zip PedestrianPlugin.zip PedestrianPlugin/bin/PedestrianPlugin PedestrianPlugin/manifest.json
-ln -s ../bin TimPlugin/bin
-zip TimPlugin.zip TimPlugin/bin/TimPlugin TimPlugin/manifest.json
-ln -s ../bin CARMACloudPlugin/bin
-zip CARMACloudPlugin.zip CARMACloudPlugin/bin/CARMACloudPlugin CARMACloudPlugin/manifest.json
-ln -s ../bin PortDrayagePlugin/bin
-zip PortDrayagePlugin.zip PortDrayagePlugin/bin/PortDrayagePlugin PortDrayagePlugin/manifest.json
-ln -s ../bin ODELoggerPlugin/bin
-zip ODELoggerPlugin.zip ODELoggerPlugin/bin/ODELoggerPlugin ODELoggerPlugin/manifest.json
-ln -s ../bin CARMAStreetsPlugin/bin
-zip CARMAStreetsPlugin.zip CARMAStreetsPlugin/bin/CARMAStreetsPlugin CARMAStreetsPlugin/manifest.json
+for PLUGIN in "${PLUGINS[@]}"; do
+    ln -s ../bin "$PLUGIN"/bin
+    zip "$PLUGIN".zip "$PLUGIN"/bin/"$PLUGIN" "$PLUGIN"/manifest.json
+done
