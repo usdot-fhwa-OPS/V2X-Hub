54d5202d1d8d2f5dbfbaace243612a0ef6d1ae38
Cody Garver <dylan.garver@leidos.com>
2022-12-16 11:17:00 -0500

    Move apt dependencies to variable and alphabetize

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,26 +5,27 @@ set -e
 
 apt-get update
 
-# install all things needed for deployment, always done
-apt-get install -y \
+DEPENDENCIES="build-essential \
     cmake \
     git \
-    build-essential \
-    libgtest-dev \
-    libssl-dev \
-    qtbase5-dev \
-    zip \
-    libmysqlcppconn-dev \
     libboost1.71-all-dev \
-    libmysqlclient-dev \
-    uuid-dev \
-    zlib1g \
-    libxerces-c-dev \
+    libcpprest-dev \
     libcurl4-openssl-dev \
-    libgps-dev \
-    libsnmp-dev \
-    librdkafka-dev \
-    libjsoncpp-dev \
     libev-dev \
+    libgps-dev \
+    libgtest-dev \
+    libjsoncpp-dev \
+    libmysqlclient-dev \
+    libmysqlcppconn-dev \
+    librdkafka-dev \
+    libsnmp-dev \
+    libssl-dev \
     libuv1-dev \
-    libcpprest-dev
+    libxerces-c-dev \
+    qtbase5-dev \
+    uuid-dev \
+    zip \
+    zlib1g"
+
+# install all things needed for deployment, always done
+apt-get install -y $DEPENDENCIES
