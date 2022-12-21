85b5554ab2df95233c7c33dbd1a7f9a27ce0d886
Rob Baily <rob.baily@leidos.com>
2022-12-21 14:54:01 -0500

    Push changes that actually remove libwebsockets custom build

--- ext/build.sh
+++ ext/build.sh
@@ -3,32 +3,23 @@
 # exit on errors
 set -e
 
-# The V2X Hub supplied plugins have a dependency on a version of libwebsockets that is newer than the installable package that comes with Ubuntu. Hence a custom version of the software has benn forked and made available with V2X-Hub.
-pushd /tmp
-#git clone https://github.com/usdot-fhwa-OPS/libwebsockets.git
-wget -O libwebsockets-v4.2.0.tar.gz https://github.com/warmcat/libwebsockets/archive/refs/tags/v4.2.0.tar.gz
-tar xvf libwebsockets-v4.2.0.tar.gz
-cd libwebsockets-4.2.0/
-cmake -DLWS_WITH_SHARED=OFF -DLWS_SUPPRESS_DEPRECATED_API_WARNINGS=ON .
-make
-make install
-popd
-
 # An OPENAPI based Qt webservice is needed by the plugins for http requests processing. A custom generated code using OPENAPI framework is located in GitHub.
 pushd /tmp
 #git clone https://github.com/usdot-fhwa-OPS/qhttpengine.git
-wget -O qhttpengine-1.0.1.tar.gz https://github.com/nitroshare/qhttpengine/archive/refs/tags/1.0.1.tar.gz
-tar xvf qhttpengine-1.0.1.tar.gz
-cd qhttpengine-1.0.1/
+QHTTPENGINE_VERSION=1.0.1
+wget -O qhttpengine-${QHTTPENGINE_VERSION}.tar.gz https://github.com/nitroshare/qhttpengine/archive/refs/tags/${QHTTPENGINE_VERSION}.tar.gz
+tar xvf qhttpengine-${QHTTPENGINE_VERSION}.tar.gz
+cd qhttpengine-${QHTTPENGINE_VERSION}/
 cmake .
 make 
 make install
 popd
 
 pushd /tmp
-wget -O date-v3.0.1.tar.gz https://github.com/HowardHinnant/date/archive/refs/tags/v3.0.1.tar.gz
-tar xvf date-v3.0.1.tar.gz
-cd date-3.0.1/
+DATELIB_VERSION=3.0.1
+wget -O date-${DATELIB_VERSION}.tar.gz https://github.com/HowardHinnant/date/archive/refs/tags/v${DATELIB_VERSION}.tar.gz
+tar xvf date-${DATELIB_VERSION}.tar.gz
+cd date-${DATELIB_VERSION}/
 cmake .
 make
 make install
