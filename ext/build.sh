3d7a6ae5d875a5ba2e6d701f18e20f4011ea3386
dan-du-car <dan.du@leidos.com>
2022-12-30 17:45:07 -0500

    update configure for netsnmp

--- ext/build.sh
+++ ext/build.sh
@@ -59,7 +59,7 @@ pushd /tmp
 wget http://sourceforge.net/projects/net-snmp/files/net-snmp/5.9.1/net-snmp-5.9.1.tar.gz
 tar -xvzf net-snmp-5.9.1.tar.gz
 cd net-snmp-5.9.1
-./configure 
+./configure --with-default-snmp-version="3" --with-sys-contact="@@no.where" --with-sys-location="Unknown" --with-logfile="/var/log/snmpd.log" --with-persistent-directory="/var/net-snmp"
 make
 make install
 popd
