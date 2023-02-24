3e11f612a44bf11453eef74bb70b1c801a030315
jwillmartin <jw.martin@outlook.com>
2023-02-24 09:51:53 -0500

    modified:   configuration/arm64/initialization.sh

--- configuration/arm64/initialization.sh
+++ configuration/arm64/initialization.sh
@@ -2,10 +2,11 @@
 sudo apt-get -y remove docker docker-engine docker.io containerd runc
 sudo apt-get update
 sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io 
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo apt -y install python3-pip
 sudo pip3 install docker-compose
+sudo apt-get install mariadb-client-10.5
 sudo docker-compose up -d
