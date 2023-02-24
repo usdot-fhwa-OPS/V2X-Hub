f9196b8937cee87ca9df59fb4c55c5fcb855407d
jwillmartin <jw.martin@outlook.com>
2023-02-24 14:29:34 -0500

    modified:   configuration/arm64/initialization.sh 	modified:   configuration/arm64/mysql/add_v2xhub_user.bash

--- configuration/arm64/initialization.sh
+++ configuration/arm64/initialization.sh
@@ -2,8 +2,10 @@
 sudo apt-get -y remove docker docker-engine docker.io containerd runc
 sudo apt-get update
 sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+OS=$(lsb_release -i | awk 'FS=":" {print $3;}' | awk '{print tolower($0)}')
+arch=$(dpkg --print-architecture)
+curl -fsSL https://download.docker.com/linux/$OS/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=$arch] https://download.docker.com/linux/$OS $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo apt -y install python3-pip
