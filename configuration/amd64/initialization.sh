b410da295411dfab3ffb7c183a089b58aa095b18
snuc <andrew.w.gaines@leidos.com>
2023-06-30 07:48:59 -0400

    updated initialization script for amd64 and arm64. script now installs necessary dependencies, creates secret files, prompts user to input mysql passwords, and create v2xhub user

--- configuration/amd64/initialization.sh
+++ configuration/amd64/initialization.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
+
+# update and upgrade commands to update linux OS
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt update -y && sudo apt upgrade -y
+
+#installing necessary and useful apps
+sudo apt-get install chromium-browser -y      #Chrome required for CARMA platform/V2X Hub UI(?)
+sudo apt install curl -y                      #Curl for downloading files over internet
+
+#install docker
+curl -L https://raw.githubusercontent.com/usdot-fhwa-stol/carma-platform/develop/engineering_tools/install-docker.sh | bash 
+
+#make passwords for mysql
+mkdir secrets && cd secrets
+sleep 1
+
+#creates password files where user inputs password
+read -p "enter password for the mysql_root_password: " sql_root_pass
+echo "$sql_root_pass" > sql_root_pass.txt
+
+read -p "enter password for mysql_password: " sql_pass
+echo "$sql_pass" > sql_pass.txt
+
+#remove endline characters from password files
+tr -d '\n' <sql_root_pass.txt> mysql_root_password.txt && tr -d '\n' <sql_pass.txt> mysql_password.txt
+rm sql_root_pass.txt && rm sql_pass.txt
+
+#AMD64 initialzation
 sudo apt-get -y remove docker docker-engine docker.io containerd runc
 sudo apt-get update
 sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
@@ -10,3 +38,13 @@ sudo apt -y install python3-pip
 sudo pip3 install docker-compose
 sudo docker-compose pull
 sudo docker-compose up -d
+sudo apt-get update -y && sudo apt update -y
+
+#create v2xhub user
+cd mysql
+./add_v2xhub_user.bash
+
+sudo apt-get update -y && sudo apt update -y
+
+echo -e "\n open chromium-browser, go to https://127.0.0.1:19760, accept certs, and go to https://127.0.0.1 to login to V2x hub."
+sleep 3
