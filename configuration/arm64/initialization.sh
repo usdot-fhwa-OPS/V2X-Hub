5f771fe4b8f325f31620622a6879cc9689661acb
Andy <109987630+gainesaw@users.noreply.github.com>
2023-07-05 11:42:12 -0400

    Update initialization.sh arm64
    same changes as amd64, removed duplicate apt-get update lines

--- configuration/arm64/initialization.sh
+++ configuration/arm64/initialization.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # update and upgrade commands to update linux OS
-sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt update -y && sudo apt upgrade -y
 
 #installing necessary and useful apps
@@ -27,16 +26,15 @@ rm sql_root_pass.txt && rm sql_pass.txt
 #ARM initialization
 cd ..
 sudo apt-get -y remove docker docker-engine docker.io containerd runc
-sudo apt-get update
 sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 OS=$(lsb_release -i | awk 'FS=":" {print $3;}' | awk '{print tolower($0)}')
 arch=$(dpkg --print-architecture)
 curl -fsSL https://download.docker.com/linux/$OS/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=$arch] https://download.docker.com/linux/$OS $(lsb_release -cs) stable"
-sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo apt -y install python3-pip
 sudo pip3 install docker-compose
+sudo apt update -y && sudo apt upgrade -y
 sudo docker-compose up -d
 
 #create v2xhub user
