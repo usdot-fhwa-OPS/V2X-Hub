1409c16e8852e65eabe11810e842c0ee8e39ccf0
dan-du-car <dan.du@leidos.com>
2023-07-06 15:47:27 +0000

    add unit test

--- src/tmx/TmxUtils/test/test_kafka_producer_worker.cpp
+++ src/tmx/TmxUtils/test/test_kafka_producer_worker.cpp
@@ -15,3 +15,18 @@ TEST(test_kafka_producer_worker, create_producer)
     worker->send(msg);
     worker->stop();
 }
+
+TEST(test_kafka_producer_worker, create_producer_no_topic)
+{
+    std::string broker_str = "localhost:9092";
+    std::string topic = "test";
+    auto client = std::make_shared<tmx::utils::kafka_client>();
+    std::shared_ptr<tmx::utils::kafka_producer_worker> worker;
+    worker = client->create_producer(broker_str);
+    worker->init();
+    worker->printCurrConf();
+    std::string msg = "test message";
+    // // Run this unit test without launching kafka broker will throw connection refused error
+    worker->send(msg, topic);
+    worker->stop();
+}
