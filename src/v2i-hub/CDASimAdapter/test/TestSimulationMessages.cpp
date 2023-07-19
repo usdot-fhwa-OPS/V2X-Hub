9a534b3475188a3cada25207cc4152a7fbd86f47
dan-du-car <dan.du@leidos.com>
2023-07-19 15:12:29 +0000

    use static func

--- src/v2i-hub/CDASimAdapter/test/TestSimulationMessages.cpp
+++ src/v2i-hub/CDASimAdapter/test/TestSimulationMessages.cpp
@@ -12,8 +12,7 @@ TEST(SimulationMessages, ExternalObjectToRoutableMessage)
 {
     simulation::ExternalObject externalObj;
     string expectedStr = "{\"metadata\":{\"is_simulation\":false,\"datum\":\"\",\"proj_string\":\"\",\"sensor_x\":0.0,\"sensor_y\":0.0,\"sensor_z\":0.0,\"infrastructure_id\":\"\",\"sensor_id\":\"\"},\"header\":{\"seq\":0,\"stamp\":{\"secs\":0,\"nsecs\":0}},\"id\":0,\"pose\":{\"pose\":{\"position\":{\"x\":0.0,\"y\":0.0,\"z\":0.0},\"orientation\":{\"x\":0.0,\"y\":0.0,\"z\":0.0,\"w\":0.0}},\"covariance\":[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]},\"velocity\":{\"twist\":{\"linear\":{\"x\":0.0,\"y\":0.0,\"z\":0.0},\"angular\":{\"x\":0.0,\"y\":0.0,\"z\":0.0}},\"covariance\":[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]},\"size\":{\"x\":0.0,\"y\":0.0,\"z\":0.0},\"confidence\":0.0,\"object_type\":\"\",\"dynamic_obj\":false}";
-    tmx::utils::sim::SimulationExternalObjectConverter converter;
-    converter.jsonToSimExternalObj(expectedStr, externalObj);
+    tmx::utils::sim::SimulationExternalObjectConverter::jsonToSimExternalObj(expectedStr, externalObj);
     ASSERT_EQ("ExternalObject", std::string(simulation::ExternalObject::MessageSubType));
     ASSERT_EQ("Simulation", std::string(simulation::ExternalObject::MessageType));
     tmx::routeable_message routeableMsg;
