0db0b59922a666b4d726466d3521636c9bb49ddd
dan-du-car <dan.du@leidos.com>
2023-07-27 14:37:39 +0000

    add comments to python script

--- src/v2i-hub/CDASimAdapter/scripts/udp_socket_server.py
+++ src/v2i-hub/CDASimAdapter/scripts/udp_socket_server.py
@@ -1,5 +1,12 @@
 import socket
-
+'''
+Purpose: This script is to launch a UDP server and used to test the CDASimAdapter simulation registration.
+Usage: Run this script first, it shall open a socket listenning to port 6767.
+    Launch the v2xhub and enable the CDASimAdapter plugin. Upon the plugin startup,
+    it sends registration message to port 6767. This UDP server shall receive the registration message,
+    and print this message on the terminal.
+Run command: python3 udp_socket_server.py
+'''
 UDP_IP = "127.0.0.1"
 UDP_SOCKET_PORT_SIM_REGISTRATION = 6767
 
