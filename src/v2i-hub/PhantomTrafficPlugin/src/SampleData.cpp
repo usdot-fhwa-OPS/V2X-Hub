7d30ef8200ab455dd891842aeb057fb2d2b4bb73
Ishan Joshi <joshi.ishan23@gmail.com>
2024-02-07 18:52:56 -0800

    Added new PhantomTrafficPlugin

---
+++ src/v2i-hub/PhantomTrafficPlugin/src/SampleData.cpp
@@ -0,0 +1,25 @@
+/*
+ * SampleData.cpp
+ *
+ *  Created on: May 16, 2016
+ *      Author: ivp
+ */
+
+#include "SampleData.h"
+
+namespace PhantomTrafficPlugin {
+
+// This class is used to demonstrate how code could be shared between
+// a plugin (in the src folder) and the unit tests (in the test folder).
+// See header file.
+
+SampleData::SampleData() :
+	Value(456)
+{
+}
+
+SampleData::~SampleData()
+{
+}
+
+} /* namespace PhantomTrafficPlugin */
