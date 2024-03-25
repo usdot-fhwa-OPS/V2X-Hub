12d665aebddcc12b1239338f75e840ee6988c549
jwillmartin <jw.martin@outlook.com>
2024-03-25 17:08:49 -0400

    modified:   configuration/arm64/docker-compose.yml 	modified:   configuration/arm64/initialization.sh

--- configuration/arm64/initialization.sh
+++ configuration/arm64/initialization.sh
@@ -35,14 +35,20 @@ fi
 
 #ARM initialization
 cd $directory
-sudo apt-get -y remove docker docker-engine docker.io containerd runc
-sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-OS=$(lsb_release -i | awk 'FS=":" {print $3;}' | awk '{print tolower($0)}')
-arch=$(dpkg --print-architecture)
-curl -fsSL https://download.docker.com/linux/$OS/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=$arch] https://download.docker.com/linux/$OS $(lsb_release -cs) stable"
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo apt update -y && sudo apt upgrade -y
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+# Add Docker's official GPG key:
+sudo apt-get -y install ca-certificates
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo docker compose up -d
 
 #create v2xhub user
