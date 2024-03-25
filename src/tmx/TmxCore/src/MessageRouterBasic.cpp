b22acc1d41847a3ec0c22ef55d394e2d2f44e7a7
dev <paul.k.bourelly@leidos.com>
2024-03-25 10:07:49 -0400

    Remove unnecessary volatile keywork

--- src/tmx/TmxCore/src/MessageRouterBasic.cpp
+++ src/tmx/TmxCore/src/MessageRouterBasic.cpp
@@ -52,8 +52,7 @@ void MessageRouterBasic::broadcastMessage(MessageReceiver *sender, IvpMessage *m
 
 	pthread_mutex_lock(&this->mActiveBroadcastsLock);
 	// Volatile type should not be used in compound operations cpp:S6191
-	auto val = this->mActiveBroadcasts;
-	this->mActiveBroadcasts = val + 1;
+	this->mActiveBroadcasts++;
 	pthread_mutex_unlock(&this->mActiveBroadcastsLock);
 
 	int broadcastCount = 0;
@@ -107,10 +106,10 @@ void MessageRouterBasic::broadcastMessage(MessageReceiver *sender, IvpMessage *m
 			}
 		}
 	}
+	
 	// Volatile type should not be used in compound operations cpp:S6191
 	pthread_mutex_lock(&this->mActiveBroadcastsLock);
-	auto val = this->mActiveBroadcasts;
-	this->mActiveBroadcasts = val - 1;
+	this->mActiveBroadcasts--;
 	pthread_mutex_unlock(&this->mActiveBroadcastsLock);
 
 	//LOG_DEBUG("MessageRouterBasic::broadcastMessage for " << msg->subtype << " from "<< sender->pluginName << " Time (ms) "<<timer_ms);
