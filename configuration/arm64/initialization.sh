e10ea5087ee9b4acf60874f5118768c21a19ca28
jwillmartin <jw.martin@outlook.com>
2024-04-04 12:13:13 -0400

    modified:   configuration/amd64/docker-compose.yml 	modified:   configuration/amd64/initialization.sh 	modified:   configuration/arm64/docker-compose.yml 	modified:   configuration/arm64/initialization.sh

--- configuration/arm64/initialization.sh
+++ configuration/arm64/initialization.sh
@@ -2,17 +2,16 @@
 directory=$(pwd)
 mysqlDir="$directory/mysql"
 
-# update and upgrade commands to update linux OS
+# Update and upgrade commands to update linux OS
 sudo apt update -y && sudo apt upgrade -y
 
-#installing necessary and useful apps
-sudo apt-get install chromium-browser -y      #Chrome required for CARMA platform/V2X Hub UI(?)
-sudo apt install curl -y                      #Curl for downloading files over internet
+# Install necessary and useful apps
+sudo apt-get install chromium-browser -y
 
-#make passwords for mysql
+# Make passwords for mysql
 mkdir -p secrets && cd secrets
 
-#creates password files where user inputs password
+# Creates password files where user inputs password
 FILE1=mysql_root_password.txt
 FILE2=mysql_password.txt
 if test -f "$FILE1"; then
@@ -20,7 +19,7 @@ if test -f "$FILE1"; then
 else
     read -p "enter password for the mysql_root_password: " sql_root_pass
     echo "$sql_root_pass" > sql_root_pass.txt
-    #remove endline characters from password files
+    # Remove endline characters from password files
     tr -d '\n' <sql_root_pass.txt> mysql_root_password.txt && rm sql_root_pass.txt
 fi
 
@@ -29,29 +28,30 @@ if test -f "$FILE2"; then
 else
     read -p "enter password for mysql_password: " sql_pass
     echo "$sql_pass" > sql_pass.txt
-    #remove endline characters from password files
+    # Remove endline characters from password files
     tr -d '\n' <sql_pass.txt> mysql_password.txt && rm sql_pass.txt
 fi
 
-#ARM initialization
+# ARM64 initialization
 cd $directory
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
 # Add Docker's official GPG key:
-sudo apt-get -y install ca-certificates
+sudo apt-get update
+sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo curl -fsSL https://download.docker.com/linux/$(. /etc/os-release && echo "$ID")/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/$(. /etc/os-release && echo "$ID") \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo docker compose up -d
 
-#create v2xhub user
+# Create V2X Hub user
 cd $mysqlDir
 ./add_v2xhub_user.bash
 
