2e954d01f1acac29ed0ebfcd4ec8869315cda5c3
dan-du-car <62157949+dan-du-car@users.noreply.github.com>
2025-01-21 14:45:52 -0500

    Cda1tenth plugin bsm (#664)
    <!-- Thanks for the contribution, this is awesome. -->
    
    # PR Details
    ## Description
    Implementing the BSM handler in the CDA 1Tenth Plugin, BSMs will be
    determined based on their type. Plugin logic will then convert the BSM
    to a JSON property tree form using the “toTree” function.
    <!--- Describe your changes in detail -->
    
    ## Related Issue
    NA
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    
    ## Motivation and Context
    CDA1Tenth Support
    <!--- Why is this change required? What problem does it solve? -->
    
    ## How Has This Been Tested?
    Unit test and local integration test
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Defect fix (non-breaking change that fixes an issue)
    - [x] New feature (non-breaking change that adds functionality)
    - [ ] Breaking change (fix or feature that cause existing functionality
    to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have added any new packages to the sonar-scanner.properties file
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [x] I have read the **CONTRIBUTING** document.
    [V2XHUB Contributing
    Guide](https://github.com/usdot-fhwa-OPS/V2X-Hub/blob/develop/Contributing.md)
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.

---
+++ src/v2i-hub/CDA1TenthPlugin/src/BSMConverter.cpp
@@ -0,0 +1,225 @@
+/**
+ * Copyright (C) 2025 LEIDOS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+#include "BSMConverter.h"
+
+namespace CDA1TenthPlugin
+{
+  pt BSMConverter::toTree(const BasicSafetyMessage_t &bsm)
+  {
+    // Logic to convert from input format to tree structure
+    pt root;
+    pt bsmChild;
+    pt coreChild;
+    coreChild.put("id", toTemporaryIdString(bsm.coreData.id));
+    coreChild.put("msgCnt", bsm.coreData.msgCnt);
+    coreChild.put("secMark", bsm.coreData.secMark);
+    coreChild.put("lat", bsm.coreData.lat);
+    coreChild.put("long", bsm.coreData.Long);
+    coreChild.put("elev", bsm.coreData.elev);
+    // // Add accurracy
+    pt accuracyChild;
+    accuracyChild.put("semiMajor", bsm.coreData.accuracy.semiMajor);
+    accuracyChild.put("semiMinor", bsm.coreData.accuracy.semiMinor);
+    accuracyChild.put("orientation", bsm.coreData.accuracy.orientation);
+    coreChild.add_child("accuracy", accuracyChild);
+    // Add transmission
+    pt transmissionChild;
+    transmissionChild.put(toTransmissionString(bsm.coreData.transmission), "");
+    coreChild.add_child("transmission", transmissionChild);
+    coreChild.put("speed", bsm.coreData.speed);
+    coreChild.put("heading", bsm.coreData.heading);
+    coreChild.put("angle", bsm.coreData.angle);
+    // // Add accelSet
+    pt accelSetChild;
+    accelSetChild.put("yaw", bsm.coreData.accelSet.yaw);
+    accelSetChild.put("long", bsm.coreData.accelSet.Long);
+    accelSetChild.put("lat", bsm.coreData.accelSet.lat);
+    accelSetChild.put("vert", bsm.coreData.accelSet.vert);
+    coreChild.add_child("accelSet", accelSetChild);
+    // // Add brakes
+    pt brakesChild;
+    brakesChild.put("wheelBrakes", toBrakeAppliedStatusString(bsm.coreData.brakes.wheelBrakes));
+    // Add traction child
+    pt tractionChild;
+    tractionChild.put(toTractionString(bsm.coreData.brakes.traction), "");
+    brakesChild.add_child("traction", tractionChild);
+    // Add abs child
+    pt absChild;
+    absChild.put(toAntiLockBrakeStatusString(bsm.coreData.brakes.abs), "");
+    brakesChild.add_child("abs", absChild);
+    // Add scs child
+    pt scsChild;
+    scsChild.put(toStabilityControlStatusString(bsm.coreData.brakes.scs), "");
+    brakesChild.add_child("scs", scsChild);
+    // Add brakeBoost child
+    pt brakeBoostChild;
+    brakeBoostChild.put(toBrakeBoostAppliedString(bsm.coreData.brakes.brakeBoost), "");
+    brakesChild.add_child("brakeBoost", brakeBoostChild);
+    // Add auxBrakes child
+    pt auxBrakesChild;
+    auxBrakesChild.put(toAuxiliaryBrakeStatusString(bsm.coreData.brakes.auxBrakes), "");
+    brakesChild.add_child("auxBrakes", auxBrakesChild);
+    coreChild.add_child("brakes", brakesChild);
+    // // Add size
+    pt sizeChild;
+    sizeChild.put("width", bsm.coreData.size.width);
+    sizeChild.put("length", bsm.coreData.size.length);
+    coreChild.add_child("size", sizeChild);
+    bsmChild.add_child("coreData", coreChild);
+    root.add_child("BasicSafetyMessage", bsmChild);
+    return root;
+  }
+
+  string BSMConverter::toTransmissionString(const TransmissionState_t &transmission)
+  {
+    switch (transmission)
+    {
+    case TransmissionState::TransmissionState_neutral:
+      return "neutral";
+    case TransmissionState::TransmissionState_park:
+      return "park";
+    case TransmissionState::TransmissionState_forwardGears:
+      return "forwardGears";
+    case TransmissionState::TransmissionState_reverseGears:
+      return "reverseGears";
+    case TransmissionState::TransmissionState_reserved1:
+      return "reserved1";
+    case TransmissionState::TransmissionState_reserved2:
+      return "reserved2";
+    case TransmissionState::TransmissionState_reserved3:
+      return "reserved3";
+    case TransmissionState::TransmissionState_unavailable:
+      return "unavailable";
+    default:
+      return "unknown";
+    }
+  }
+
+  string BSMConverter::toTractionString(const TractionControlStatus_t &traction)
+  {
+    switch (traction)
+    {
+    case TractionControlStatus::TractionControlStatus_unavailable:
+      return "unavailable";
+    case TractionControlStatus::TractionControlStatus_off:
+      return "off";
+    case TractionControlStatus::TractionControlStatus_on:
+      return "on";
+    case TractionControlStatus::TractionControlStatus_engaged:
+      return "engaged";
+    default:
+      return "unknown";
+    }
+  }
+
+  string BSMConverter::toAntiLockBrakeStatusString(const AntiLockBrakeStatus_t &abs)
+  {
+    switch (abs)
+    {
+    case AntiLockBrakeStatus::AntiLockBrakeStatus_unavailable:
+      return "unavailable";
+    case AntiLockBrakeStatus::AntiLockBrakeStatus_off:
+      return "off";
+    case AntiLockBrakeStatus::AntiLockBrakeStatus_on:
+      return "on";
+    case AntiLockBrakeStatus::AntiLockBrakeStatus_engaged:
+      return "engaged";
+    default:
+      return "unknown";
+    }
+  }
+
+  string BSMConverter::toStabilityControlStatusString(const StabilityControlStatus_t &scs)
+  {
+    switch (scs)
+    {
+    case StabilityControlStatus::StabilityControlStatus_unavailable:
+      return "unavailable";
+    case StabilityControlStatus::StabilityControlStatus_off:
+      return "off";
+    case StabilityControlStatus::StabilityControlStatus_on:
+      return "on";
+    case StabilityControlStatus::StabilityControlStatus_engaged:
+      return "engaged";
+    default:
+      return "unknown";
+    }
+  }
+
+  string BSMConverter::toBrakeBoostAppliedString(const BrakeBoostApplied_t &brakeBoost)
+  {
+    switch (brakeBoost)
+    {
+    case BrakeBoostApplied::BrakeBoostApplied_unavailable:
+      return "unavailable";
+    case BrakeBoostApplied::BrakeBoostApplied_off:
+      return "off";
+    case BrakeBoostApplied::BrakeBoostApplied_on:
+      return "on";
+    default:
+      return "unknown";
+    }
+  }
+
+  string BSMConverter::toAuxiliaryBrakeStatusString(const AuxiliaryBrakeStatus_t &auxBrakes)
+  {
+    switch (auxBrakes)
+    {
+    case AuxiliaryBrakeStatus::AuxiliaryBrakeStatus_unavailable:
+      return "unavailable";
+    case AuxiliaryBrakeStatus::AuxiliaryBrakeStatus_off:
+      return "off";
+    case AuxiliaryBrakeStatus::AuxiliaryBrakeStatus_on:
+      return "on";
+    case AuxiliaryBrakeStatus::AuxiliaryBrakeStatus_reserved:
+      return "reserved";
+    default:
+      return "unknown";
+    }
+  }
+
+  string BSMConverter::toBrakeAppliedStatusString(const BrakeAppliedStatus_t &wheelBrakes)
+  {
+    std::string result;
+    for (int i = 0; i < 5; ++i)
+    {
+      result += (wheelBrakes.buf[i / 8] & (1 << (7 - (i % 8)))) ? '1' : '0';
+    }
+    return result;
+  }
+
+  string BSMConverter::toTemporaryIdString(const TemporaryID_t &id)
+  {
+    std::stringstream id_fill_ss;
+    for (int i = 0; i < id.size; i++)
+    {
+      id_fill_ss << std::setfill('0') << std::setw(2) << std::hex << (int)id.buf[i];
+    }
+    return id_fill_ss.str();
+  }
+
+  string BSMConverter::toJsonString(const pt &tree)
+  {
+    std::ostringstream buf;
+    write_json(buf, tree, false);
+    std::string jsonStr = buf.str();
+    boost::algorithm::erase_all(jsonStr, "\n");
+    boost::algorithm::erase_all(jsonStr, "\t");
+    boost::algorithm::erase_all(jsonStr, " ");
+    return jsonStr;
+    return buf.str();
+  }
+}
