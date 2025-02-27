efdd9bb0de163489f88a94556f81eef2d478962f
paulbourelly999 <77466294+paulbourelly999@users.noreply.github.com>
2025-02-27 12:43:31 -0500

    Update deployment_dependencies.sh

--- scripts/deployment_dependencies.sh
+++ scripts/deployment_dependencies.sh
@@ -3,8 +3,11 @@
 # exit on errors
 set -e
 
+# Get ubuntu distribution code name. All STOL APT debian packages are pushed to S3 bucket based on distribution codename.
+. /etc/lsb-release
+
 # add the STOL APT repository
-echo "deb [trusted=yes] http://s3.amazonaws.com/stol-apt-repository develop main" > /etc/apt/sources.list.d/stol-apt-repository.list
+echo "deb [trusted=yes] http://s3.amazonaws.com/stol-apt-repository develop ${DISTRIB_CODENAME}" > /etc/apt/sources.list.d/stol-apt-repository.list
 
 apt-get update
 
