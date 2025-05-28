5b66f021e7969cdb50bffe37752fd893e62091b9
paulbourelly999 <77466294+paulbourelly999@users.noreply.github.com>
2025-05-28 15:53:47 -0400

    Add Documentation for CARMA Cloud Plugin (#716)
    <!-- Thanks for the contribution, this is awesome. -->
    
    # PR Details
    ## Description
    Update documentation for CARMA Cloud Plugin 
    <!--- Describe your changes in detail -->
    
    ## Related Issue
    
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    
    ## Motivation and Context
    [ARC-365](https://usdot-carma.atlassian.net/browse/ARC-365)
    <!--- Why is this change required? What problem does it solve? -->
    
    ## How Has This Been Tested?
    NA
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Defect fix (non-breaking change that fixes an issue)
    - [ ] New feature (non-breaking change that adds functionality)
    - [ ] Breaking change (fix or feature that cause existing functionality
    to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have added any new packages to the sonar-scanner.properties file
    - [ ] My change requires a change to the documentation.
    - [x] I have updated the documentation accordingly.
    - [x] I have read the **CONTRIBUTING** document.
    [V2XHUB Contributing
    Guide](https://github.com/usdot-fhwa-OPS/V2X-Hub/blob/develop/Contributing.md)
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.
    
    
    [ARC-365]:
    https://usdot-carma.atlassian.net/browse/ARC-365?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/v2i-hub/CARMACloudPlugin/src/CARMACloudPlugin.h
+++ src/v2i-hub/CARMACloudPlugin/src/CARMACloudPlugin.h
@@ -174,7 +174,6 @@ private:
 	pthread_mutex_t _settingsMutex = PTHREAD_MUTEX_INITIALIZER;
 	pthread_mutex_t _timMutex = PTHREAD_MUTEX_INITIALIZER;
 	J2735MessageFactory factory;
-	uint64_t _frequency = 0;
 	string carma_cloud_url;
 	string base_hb;
 	string base_req;
