43f652b5af1e977cbad3f76f79064f125f9bb991
paulbourelly999 <paul.k.bourelly@leidos.com>
2025-06-02 13:07:23 -0400

    UPdates

--- scripts/deployment_dependencies.sh
+++ scripts/deployment_dependencies.sh
@@ -8,7 +8,7 @@ set -ex
 
 # add the STOL APT repository
 echo "deb [trusted=yes] http://s3.amazonaws.com/stol-apt-repository develop ${DISTRIB_CODENAME}" > /etc/apt/sources.list.d/stol-apt-repository.list
-
+apt-get clean
 apt-get update --fix-missing
 
 # NOTE: libwebsockets-dev from Ubuntu 20 on is sufficient
