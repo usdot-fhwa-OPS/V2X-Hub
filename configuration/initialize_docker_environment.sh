usdot-fhwa-OPS/V2X-Hub

commit 09d7389186c153da3aefe2a9d6a00522d9b9f14f

Remove unnecessary manual configuration steps (#774)
<!-- Thanks for the contribution, this is awesome. -->

# PR Details
## Description
This PR attempts to get V2X Hub closer to a state where V2X Hub is
deployable with only a DB configuration state (localhost.sql file) and a
docker environment variable state (.env file). It attempts to do this
by:

- Replace secret files with docker env secrets
- Use deployment docker compose for dev container
- add docker env to create admin user
- Fix add_user and update_user queries
- Add script to generate certificates in case non exist 



<!--- Describe your changes in detail -->

## Related GitHub Issue
Also fixes issues described in
https://github.com/usdot-fhwa-OPS/V2X-Hub/issues/749 at the bottom. By
making the Dev Container setup use the deployment docker compose file,
it has access to any folders/volumes created under the configuration/
directory so having duplicate volumes for devcontainer and for regular
deployment is no longer necessary
<!--- This project only accepts pull requests related to open GitHub
issues or Jira Keys -->
<!--- If suggesting a new feature or change, please discuss it in an
issue first -->
<!--- If fixing a bug, there should be an issue describing it with steps
to reproduce -->
<!--- Please DO NOT name partially fixed issues, instead open an issue
specific to this fix -->
<!--- Please link to the issue here: -->

## Related Jira Key
NA
<!-- e.g. CAR-123 -->

## Motivation and Context
Improve deployment by reducing manual steps necessary for deployment and
limiting all configuration to DB state and docker environment state
<!--- Why is this change required? What problem does it solve? -->

## How Has This Been Tested?
Local integration testing with Dev Container. 

**Steps to test regular deployment**

1. Navigate to `configuration/`
2. Clear any certificates in `configuration/ssl/` directory
3. Run V2X via `configuration/initialization.sh` script
4. Ensure V2X Hub UI deploys and is accessible

configuration/initialize_docker_environment.sh
@@ -9,7 +9,6 @@ SIMULATION_MODE_DEFAULT="FALSE"
 SIMULATION_IP_DEFAULT="127.0.0.1"
 SENSOR_JSON_FILE_PATH_DEFAULT="/var/www/plugins/MAP/sensors.json"
 COMPOSE_PROFILES=""
-
 echo "Initializing Docker Environment for V2X Hub..."
 # Retrieve available release candidates
 release_candidates=$(git branch -r | grep 'origin/release/' | sed 's|origin/||' | sed 's/release\//release-/g')
@@ -72,7 +71,27 @@ if [[ $SIMULATION_MODE == "TRUE" ]]; then
     read -r -p "Enter Sensor Configuration File Path (or press Enter to use default as $SENSOR_JSON_FILE_PATH_DEFAULT): " SENSOR_JSON_FILE_PATH
     SENSOR_JSON_FILE_PATH=${SENSOR_JSON_FILE_PATH:-$SENSOR_JSON_FILE_PATH_DEFAULT}
 fi
-
+# MySQL (IVP) User Password
+read -r -s -p "MYSQL PASSWORD (password for configuration database): " MYSQL_PASSWORD
+MYSQL_PASSWORD=${MYSQL_PASSWORD}
+# V2X Hub Username
+read -r -p "V2X Hub Admin Username (or press Enter to use default as v2xadmin): " V2XHUB_USERNAME
+V2XHUB_USERNAME=${V2XHUB_USERNAME:-v2xadmin}
+# V2X Hub Password
+read -r -s -p "V2X Hub Admin Password (input will be hidden): " V2XHUB_PASSWORD
+if [ $PASS_LENGTH -ge 8 ] && echo $V2XHUB_PASSWORD | grep -q [a-z] && echo $V2XHUB_PASSWORD | grep -q [A-Z] && echo $V2XHUB_PASSWORD | grep -q [0-9] && ( echo $V2XHUB_PASSWORD | grep -q [\$\!\.\+_\*@\#\^%\?~] || echo $V2XHUB_PASSWORD | grep -q [-] ); then
+    echo
+    read -s -p "Confirm password: " CONF_PASS
+    while [ $CONF_PASS != $V2XHUB_PASSWORD ]; do
+        echo
+        read -s -p "Passwords do not match. Please re-enter password: " CONF_PASS
+    done
+    echo "VALID PASSWORD"
+else
+    echo "INVALID PASSWORD"
+    echo "Password must be 8-12 charcters, and contain at least one of each of the following: uppercase letter, lowercase letter, number, and symbol"
+    exit 1
+fi
 echo "WARNING: This will overwrite the existing .env file if it exists."
 read -r -p "Are you sure you want to continue? (Y/N): " overwrite_confirm
 if [[ "$overwrite_confirm" =~ [yY](es)* ]]; then
@@ -85,6 +104,9 @@ V2XHUB_IP="$V2XHUB_IP"
 SIMULATION_MODE=$SIMULATION_MODE
 COMPOSE_PROFILES="$COMPOSE_PROFILES"
 SENSOR_JSON_FILE_PATH="$SENSOR_JSON_FILE_PATH"
+MYSQL_PASSWORD="$MYSQL_PASSWORD"
+V2XHUB_USERNAME="$V2XHUB_USERNAME"
+V2XHUB_PASSWORD="$V2XHUB_PASSWORD"
 EOF
 
     # Adding Simulation IP and Sensor Path if Simulation Mode is TRUE

container/service.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 wait-for-it.sh 127.0.0.1:3306
 
-cd /var/log/tmx
 # Add stol apt repository libraries to path for tmxcore
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/carma/lib/
 for plugin in /usr/local/plugins/*.zip; do
     echo "Installing plugin $plugin"
     tmxctl --plugin-install "$plugin"
 done
-
+cd container/
+# Generate self-signed certificates if they do not already exist
+./generate_certificates.sh
 # command plugin must always be enabled
 tmxctl --plugin CommandPlugin --enable
 # If in simulation mode, enable SimulationAdapter
@@ -17,4 +18,12 @@ then
     echo "Enabling CDASim Adapter for Simulation Integration!"
     tmxctl --plugin CDASimAdapter --enable
 fi
+# If V2XHUB_USER and V2XHUB_PASSWORD are set, create the user
+if [ -n "$V2XHUB_USERNAME" ] && [ -n "$V2XHUB_PASSWORD" ]; then
+    echo "Creating V2X Hub Admin User: $V2XHUB_USERNAME"
+    # Add V2XHub admin user (Will not add if user already exists)
+    tmxctl --user-add --username "$V2XHUB_USERNAME" --password "$V2XHUB_PASSWORD" --access-level 3
+fi
+# Start Tmx Core
 tmxcore
+

.devcontainer/docker-compose-vscode.yml
@@ -4,120 +4,15 @@ services:
       context: ..
       dockerfile: ./Dockerfile
       target: build-environment
-    container_name: v2xhub
-    network_mode: host
     privileged: true
     cap_add:
       - SYS_PTRACE
     volumes:
       # This is where VS Code should expect to find your project's source code and the value of "workspaceFolder" in .devcontainer/devcontainer.json
       - ..:/workspace:cached
-      - ./logs:/var/log/tmx
-      - ./MAP:/var/www/plugins/MAP
-      - /etc/localtime:/etc/localtime:ro
-      - /etc/timezone:/etc/timezone:ro
-      - ./ssl:/var/www/plugins/ssl:ro
-
     # Overrides default command so things don't shut down after the process ends.
     command: /bin/sh -c "while sleep 10000; do :; done"  
-    environment:
-      MYSQL_PASSWORD: /run/secrets/mysql_password
-      SIMULATION_MODE: "false"
-      # Environment variables only requires in simulation 
-      # SIMULATION_IP: 127.0.0.1
-      # SIMULATION_REGISTRATION_PORT: 6767
-      # TIME_SYNC_TOPIC: time_sync
-      # TIME_SYNC_PORT: 7575
-      # SIM_V2X_PORT: 5757
-      # SIM_INTERACTION_PORT: 7576
-      # V2X_PORT: 8686
-      LOCAL_IP: 127.0.0.1
-      INFRASTRUCTURE_ID: rsu_1234
-      INFRASTRUCTURE_NAME: East
-      SENSOR_JSON_FILE_PATH: /var/www/plugins/MAP/sensors.json
-    secrets:
-      - mysql_password
 
   db:
-    image: mysql:8.0
-    container_name: mysql
-    restart: always
-    environment:
-      MYSQL_DATABASE: IVP
-      MYSQL_USER: IVP
-      MYSQL_PASSWORD_FILE: /run/secrets/mysql_password
-      MYSQL_ROOT_PASSWORD_FILE: /run/secrets/mysql_root_password
-    network_mode: host
-    secrets:
-      - mysql_password
-      - mysql_root_password
     volumes:
-      - ../configuration/mysql/localhost.sql:/docker-entrypoint-initdb.d/localhost.sql
-      # include creation of a user (user/user) to be able to log in
-      - ./mysql_add_dev_user.sql:/docker-entrypoint-initdb.d/mysql_add_dev_user.sql
-      - mysql-datavolume:/var/lib/mysql
-
-  php:
-    image: usdotfhwaops/php:develop
-    container_name: php
-    network_mode: host
-    restart: always
-    volumes:
-      - ./ssl:/etc/apache2/ssl:ro
-    ports:
-      - "8080:80"
-      - "443:443"
-
-  zookeeper:
-    image: wurstmeister/zookeeper
-    container_name: zookeeper
-    ports:
-       - "2181:2182"
-    volumes:
-      - /etc/localtime:/etc/localtime:ro
-      - /etc/timezone:/etc/timezone:ro
-      - zookeeper-datavolume:/opt/zookeeper-3.4.13/data
-  kafka:
-    image: wurstmeister/kafka
-    container_name: kafka
-    depends_on: 
-      - zookeeper
-    ports:
-      - "9092:9092"
-    environment:
-      DOCKER_HOST_IP:  127.0.0.1
-      KAFKA_ADVERTISED_HOST_NAME:  127.0.0.1
-      KAFKA_ZOOKEEPER_CONNECT: zookeeper:2181
-      KAFKA_CREATE_TOPICS: "time_sync:1:1,modified_spat:1:1,v2xhub_scheduling_plan_sub:1:1,v2xhub_ssm_sub:1:1"
-      KAFKA_LOG_DIRS: "/kafka/kafka-logs"
-    volumes:
-      - /var/run/docker.sock:/var/run/docker.sock
-      - /etc/localtime:/etc/localtime:ro
-      - /etc/timezone:/etc/timezone:ro
-      - kafka-datavolume:/kafka/kafka-logs
-##############################################################
-# Kafka Owl for monitoring kafka traffic. UI can be accessed on 
-# configurable port set in performance/kowl.yaml NOTE: IS NOT 
-# NECESSARY FOR DEPLOYMENT BUT HELFUL FOR DEBUGGING! 
-################################################################
-  kowl:
-    image: quay.io/cloudhut/kowl
-    container_name: kowl
-    restart: on-failure
-    network_mode: host
-    depends_on:
-      - kafka
-    environment:
-      CONFIG_FILEPATH: /etc/kowl/kowl.yaml
-    volumes:
-      - ../kowl/kowl.yaml:/etc/kowl/kowl.yaml
-secrets:
-    mysql_password:
-        file: ../secrets/mysql_password.txt
-    mysql_root_password:
-        file: ../secrets/mysql_root_password.txt
-
-volumes:
-  mysql-datavolume:
-  kafka-datavolume:
-  zookeeper-datavolume:
+      - ../.devcontainer/mysql_add_dev_user.sql:/docker-entrypoint-initdb.d/mysql_add_dev_user.sql

configuration/add_v2xhub_user.sh
@@ -1,58 +1,0 @@
-#!/bin/bash
-set -e
-
-# Check if V2XHUB_VERSION is passed
-if [ -n "$1" ]; then
-    V2XHUB_VERSION="$1"
-else
-    # Prompt user for V2XHUB_VERSION
-    read -r -p "Enter the deployed V2X-Hub version number: " V2XHUB_VERSION
-fi
-
-echo "Adding V2X-Hub user for version: $V2XHUB_VERSION"
-
-# Ensure mysql-client is installed
-arch=$(dpkg --print-architecture)
-# TODO: Add a common mysql-client that works for ARM and AMD devices 
-
-if ! command -v mysql &>/dev/null; then
-  if [ $arch = "amd64" ]; then
-    REQUIRED_PKG="mysql-client"
-  else
-    REQUIRED_PKG="mariadb-client"
-  fi  
-  echo "No $REQUIRED_PKG found. Installing $REQUIRED_PKG."
-  sudo apt-get update
-  sudo apt-get --yes install $REQUIRED_PKG
-fi
-
-# Adds V2X-Hub user to mysql db
-read -p "Please enter a username: " USER
-echo "Password must be 8-12 charcters, and contain at least one of each of the following: uppercase letter, lowercase letter, number, and symbol."
-read -s -p "Please enter a password: "  PASS
-
-PASS_LENGTH=`echo $PASS | wc -c`
-
-if [ $PASS_LENGTH -ge 8 ] && echo $PASS | grep -q [a-z] && echo $PASS | grep -q [A-Z] && echo $PASS | grep -q [0-9] && ( echo $PASS | grep -q [\$\!\.\+_\*@\#\^%\?~] || echo $PASS | grep -q [-] ); then
-    echo
-    read -s -p "Confirm password: " CONF_PASS
-    while [ $CONF_PASS != $PASS ]; do
-        echo
-        read -s -p "Passwords do not match. Please re-enter password: " CONF_PASS
-    done
-    echo "VALID PASSWORD"
-    echo "Enter MYSQL ROOT PASSWORD: "
-    # Check if V2XHUB_VERSION is >= 7.5.0
-    if [[ "$(echo "$V2XHUB_VERSION 7.5.0" | awk '{print ($1 >= $2)}')" -eq 1 ]]; then
-        mysql -uroot -p --silent -h127.0.0.1 -e "INSERT INTO IVP.user (IVP.user.username, IVP.user.password, IVP.user.accessLevel) VALUES('$USER', SHA2('$PASS', 256), 3)"
-    elif [[ "$(echo "$V2XHUB_VERSION develop" | awk '{print ($1 == $2)}')" -eq 1 ]]; then
-        mysql -uroot -p --silent -h127.0.0.1 -e "INSERT INTO IVP.user (IVP.user.username, IVP.user.password, IVP.user.accessLevel) VALUES('$USER', SHA2('$PASS', 256), 3)"
-    else
-        mysql -uroot -p --silent -h127.0.0.1 -e "INSERT INTO IVP.user (IVP.user.username, IVP.user.password, IVP.user.accessLevel) VALUES('$USER', '$PASS', 3)"
-    fi
-    echo "V2X Hub user successfully added"
-else
-    echo "INVALID PASSWORD"
-    echo "Password must be 8-12 charcters, and contain at least one of each of the following: uppercase letter, lowercase letter, number, and symbol"
-    exit 1
-fi

configuration/docker-compose.yml
@@ -6,11 +6,10 @@ services:
       - MYSQL_DATABASE=IVP
       - MYSQL_USER=IVP
       - MYSQL_PASSWORD_FILE=/run/secrets/mysql_password
-      - MYSQL_ROOT_PASSWORD_FILE=/run/secrets/mysql_root_password
+      - MYSQL_RANDOM_ROOT_PASSWORD=yes
     network_mode: host
     secrets:
       - mysql_password
-      - mysql_root_password
     volumes:
       - ./mysql/localhost.sql:/docker-entrypoint-initdb.d/localhost.sql
       - ./mysql/port_drayage.sql:/docker-entrypoint-initdb.d/port_drayage.sql
@@ -73,6 +72,8 @@ services:
       - INFRASTRUCTURE_ID=${INFRASTRUCTURE_ID:-rsu_1234}
       - INFRASTRUCTURE_NAME=${INFRASTRUCTURE_NAME:-East Intersection}
       - SENSOR_JSON_FILE_PATH=${SENSOR_JSON_FILE_PATH:-/var/www/plugins/MAP/sensors.json}
+      - V2XHUB_USERNAME=${V2XHUB_USERNAME:-v2xadmin}
+      - V2XHUB_PASSWORD=${V2XHUB_PASSWORD}
     secrets:
       - mysql_password
     volumes:
@@ -80,7 +81,7 @@ services:
       - ./MAP:/var/www/plugins/MAP
       - /etc/localtime:/etc/localtime:ro 
       - /etc/timezone:/etc/timezone:ro 
-      - ./ssl:/var/www/plugins/ssl:ro
+      - ./ssl:/var/www/plugins/ssl:rw
   port_drayage_webservice:
     image: usdotfhwaops/port-drayage-webservice:${V2XHUB_VERSION:-develop}
     container_name: port_drayage_webservice
@@ -97,9 +98,7 @@ services:
       - /etc/timezone:/etc/timezone:ro 
 secrets:
     mysql_password:
-        file: ./secrets/mysql_password.txt
-    mysql_root_password:
-        file: ./secrets/mysql_root_password.txt
+        environment: MYSQL_PASSWORD
 
 volumes:
   mysql-datavolume:

configuration/initialization.sh
@@ -3,8 +3,6 @@ set -e
 
 # Initialize Docker Environment for V2X Hub
 ./initialize_docker_environment.sh
-# Initialize secrets
-./initialize_secrets.sh
 # Install Docker
 MIN_VERSION="2.1.0" # Example version, adjust as needed
 

configuration/initialize_secrets.sh
@@ -1,32 +1,0 @@
-#!/bin/bash
-set -e
-echo "Initializing secrets for V2X Hub..."
-# Save current path
-directory=$(pwd)
-# Make passwords for mysql
-mkdir -p secrets && cd secrets || return # SC2164 - Use return in case cd fails 
-
-# Creates password files where user inputs password
-FILE1=mysql_root_password.txt
-FILE2=mysql_password.txt
-if test -f "$FILE1"; then
-    echo "$FILE1 exists."
-else
-    read -r -p "enter password for the mysql_root_password: " sql_root_pass # SC2162 - read without -r will mangle backslashes 
-    echo "$sql_root_pass" > sql_root_pass.txt
-    # Remove endline characters from password files
-    tr -d '\n' <sql_root_pass.txt> mysql_root_password.txt && rm sql_root_pass.txt
-fi
-
-if test -f "$FILE2"; then
-    echo "$FILE2 exists."
-else
-    read -r -p "enter password for mysql_password: " sql_pass
-    echo "$sql_pass" > sql_pass.txt
-    # Remove endline characters from password files
-    tr -d '\n' <sql_pass.txt> mysql_password.txt && rm sql_pass.txt
-fi
-
-cd "$directory" || return # return in case cd fails
-
-echo "Secret Initialization Complete."

configuration/run_v2xhub.sh
@@ -13,9 +13,6 @@ sudo docker compose up -d
 # Update permissions for tmx logs created by plugins
 sudo chmod -R 777 ./logs
 
-# Create V2X Hub user
-source .env
-./add_v2xhub_user.sh "$V2XHUB_VERSION"
 
 chromium-browser localhost > /dev/null 2>&1 &
 

commit 7059b661d43b18ed1e2868a0a459314073c3b15e

Fix bugs introduced by simplified deployment (#775)
<!-- Thanks for the contribution, this is awesome. -->

# PR Details
## Description
Fix bugs introduced by  #774 

Bugs include the following result when running the initialization.sh
script
```
14:24:09 will@will-vm ~/workspace/V2X-Hub/configuration (develop) $ ./initialization.sh 
Initializing Docker Environment for V2X Hub...
Available Release Candidates (Only intended for use during release-testing):
  release-miura
  release-neon
  release-omega
  release-stingray
  release-tempest
  release-test_run
  release-timtsp
  release-vip-demo
  release-workzone-cp
Note: V2X-Hub multi architecture deployments only work for the versions 7.0 and above.
Available Release versions:
  7.0
  7.1
  7.2
  7.2.1
  7.2.2
  7.2.3
  7.3.0
  7.3.1
  7.4.0
  7.5.0
  7.5.1
  7.6.0
  7.7.0
  7.8.0
  7.8.1
  7.9.0
  7.10.0
  release-workzone-cp
Enter V2X-Hub Version (choose from the above, or press Enter to use the latest version 7.10.0): 7.10.0
Enable Port Drayage functionality (TRUE/FALSE, or press Enter to use default as FALSE): 
Enter Infrastructure id (or press Enter to use default as rsu_1234): 
Enter Infrastructure name (or press Enter to use default as East Intersection): 
Enter V2XHub IP (or press Enter to use default as 127.0.0.1): 
Simulation Mode (TRUE/FALSE, or press Enter to use default as FALSE): 
MYSQL PASSWORD (password for configuration database): V2X Hub Admin Username (or press Enter to use default as v2xadmin): 
V2X Hub Admin Password (input will be hidden): ./initialize_docker_environment.sh: line 82: [: -ge: unary operator expected
INVALID PASSWORD
Password must be 8-12 charcters, and contain at least one of each of the following: uppercase letter, lowercase letter, number, and symbol
```

and the following docker container output when generating ssl
certificates
```
v2xhub  | /usr/local/bin/service.sh: line 10: cd: container/: No such file or directory

configuration/initialize_docker_environment.sh
@@ -73,18 +73,24 @@ if [[ $SIMULATION_MODE == "TRUE" ]]; then
 fi
 # MySQL (IVP) User Password
 read -r -s -p "MYSQL PASSWORD (password for configuration database): " MYSQL_PASSWORD
-MYSQL_PASSWORD=${MYSQL_PASSWORD}
+echo
 # V2X Hub Username
 read -r -p "V2X Hub Admin Username (or press Enter to use default as v2xadmin): " V2XHUB_USERNAME
 V2XHUB_USERNAME=${V2XHUB_USERNAME:-v2xadmin}
+echo "Password must be 8-12 charcters, and contain at least one of each of the following: uppercase letter, lowercase letter, number, and symbol."
+
 # V2X Hub Password
 read -r -s -p "V2X Hub Admin Password (input will be hidden): " V2XHUB_PASSWORD
-if [ $PASS_LENGTH -ge 8 ] && echo $V2XHUB_PASSWORD | grep -q [a-z] && echo $V2XHUB_PASSWORD | grep -q [A-Z] && echo $V2XHUB_PASSWORD | grep -q [0-9] && ( echo $V2XHUB_PASSWORD | grep -q [\$\!\.\+_\*@\#\^%\?~] || echo $V2XHUB_PASSWORD | grep -q [-] ); then
+# Get length of the password
+PASS_LENGTH=$(echo "$PASS" | wc -c)
+# Validate password complexity
+if [ $PASS_LENGTH -ge 8 ] && echo "$V2XHUB_PASSWORD" | grep -q "[a-z]" && echo "$V2XHUB_PASSWORD" | grep -q "[A-Z]" && echo "$V2XHUB_PASSWORD" | grep -q "[0-9]" && ( echo "$V2XHUB_PASSWORD" | grep -q "[\$\!\.\+_\*@\#\^%\?~]" || echo "$V2XHUB_PASSWORD" | grep -q "[-]" ); then
     echo
-    read -s -p "Confirm password: " CONF_PASS
-    while [ $CONF_PASS != $V2XHUB_PASSWORD ]; do
+    # Confirm password
+    read -r -s -p "Confirm password: " CONF_PASS
+    while [ "$CONF_PASS" != "$V2XHUB_PASSWORD" ]; do
         echo
-        read -s -p "Passwords do not match. Please re-enter password: " CONF_PASS
+        read -r -s -p "Passwords do not match. Please re-enter password: " CONF_PASS
     done
     echo "VALID PASSWORD"
 else

container/service.sh
@@ -7,9 +7,8 @@ for plugin in /usr/local/plugins/*.zip; do
     echo "Installing plugin $plugin"
     tmxctl --plugin-install "$plugin"
 done
-cd container/
 # Generate self-signed certificates if they do not already exist
-./generate_certificates.sh
+/home/V2X-Hub/container/generate_certificates.sh
 # command plugin must always be enabled
 tmxctl --plugin CommandPlugin --enable
 # If in simulation mode, enable SimulationAdapter