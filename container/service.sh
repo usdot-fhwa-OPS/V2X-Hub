15eb3fec280e5ae7601de36ace653497b38ccf5d
paulbourelly999 <paul.k.bourelly@leidos.com>
2025-10-07 16:05:48 -0400

    Updates

--- container/service.sh
+++ container/service.sh
@@ -7,9 +7,8 @@ for plugin in /usr/local/plugins/*.zip; do
     echo "Installing plugin $plugin"
     tmxctl --plugin-install "$plugin"
 done
-cd /home/V2X-Hub/container/
 # Generate self-signed certificates if they do not already exist
-./generate_certificates.sh
+/home/V2X-Hub/container/generate_certificates.sh
 # command plugin must always be enabled
 tmxctl --plugin CommandPlugin --enable
 # If in simulation mode, enable SimulationAdapter
