631baefe8f98f04ab198703ef895effb8356533a
paulbourelly999 <paul.k.bourelly@leidos.com>
2025-10-07 09:42:20 -0400

    Updates

--- container/generate_certificates.sh
+++ container/generate_certificates.sh
@@ -7,7 +7,7 @@ if [ -f /var/www/plugins/ssl/cert.pem ] && [ -f /var/www/plugins/ssl/cert-key.pe
     echo "Certificates already exist, skipping generation"
     exit 0
 else
-    echo "Generating new self-signed certificates for V2X Hub IP $V2XHUB_IP"
-    openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout /var/www/plugins/ssl/cert-key.pem -out /var/www/plugins/ssl/cert.pem -subj "/CN=$V2XHUB_IP" -days 3650
+    echo "Generating new self-signed certificates for V2X Hub IP $LOCAL_IP"
+    openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout /var/www/plugins/ssl/cert-key.pem -out /var/www/plugins/ssl/cert.pem -subj "/CN=$LOCAL_IP" -days 3650
     chown -R plugin:www-data /var/www/plugins/ssl
 fi
--- container/service.sh
+++ container/service.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 wait-for-it.sh 127.0.0.1:3306
 
-cd /var/log/tmx
 # Add stol apt repository libraries to path for tmxcore
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/carma/lib/
 for plugin in /usr/local/plugins/*.zip; do
     echo "Installing plugin $plugin"
     tmxctl --plugin-install "$plugin"
 done
+cd container/
 # Generate self-signed certificates if they do not already exist
-generate_certificates.sh
+./generate_certificates.sh
 # command plugin must always be enabled
 tmxctl --plugin CommandPlugin --enable
 # If in simulation mode, enable SimulationAdapter
