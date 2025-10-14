16900d427f79f47737981ba72f281e5c220125a5
paulbourelly999 <paul.k.bourelly@leidos.com>
2025-10-14 12:09:35 -0400

    Updates

--- src/v2i-hub/RSUHealthMonitorPlugin/test/test_RSUHealthMonitorWorker.cpp
+++ src/v2i-hub/RSUHealthMonitorPlugin/test/test_RSUHealthMonitorWorker.cpp
@@ -1,5 +1,7 @@
-#include "RSUHealthMonitorWorker.h"
+#include <RSUHealthMonitorWorker.h>
+#include <SNMPClient.h>
 #include <gtest/gtest.h>
+
 using namespace std;
 using namespace tmx::utils;
 using namespace tmx::utils::rsu::mib::rsu41;
@@ -17,7 +19,7 @@ namespace RSUHealthMonitor
     TEST_F(test_RSUHealthMonitorWorker, GetRSUStatusConfig)
     {
         RSUStatusConfigTable rsuStatusConfigTbl = _rsuWorker->GetRSUStatusConfig(tmx::utils::rsu::RSU_SPEC::RSU_4_1);
-        ASSERT_EQ(14, rsuStatusConfigTbl.size());
+        EXPECT_EQ(14, rsuStatusConfigTbl.size());
 
     }
 
@@ -25,40 +27,38 @@ namespace RSUHealthMonitor
     {
         auto config = _rsuWorker->GetRSUStatusConfig(tmx::utils::rsu::RSU_SPEC::RSU_4_1);
         vector<string> requiredFields = {"rsuID", "rsuMibVersion", "rsuFirmwareVersion", "rsuManufacturer", "rsuGpsOutputString", "rsuMode", "rsuChanStatus"};
-        ASSERT_TRUE(_rsuWorker->validateAllRequiredFieldsPresent(config, requiredFields));
+        EXPECT_TRUE(_rsuWorker->validateAllRequiredFieldsPresent(config, requiredFields));
 
         requiredFields = {"rsuID", "rsuMibVersion", "rsuFirmwareVersion"};
-        ASSERT_FALSE(_rsuWorker->validateAllRequiredFieldsPresent(config, requiredFields));
+        EXPECT_FALSE(_rsuWorker->validateAllRequiredFieldsPresent(config, requiredFields));
     }
 
     TEST_F(test_RSUHealthMonitorWorker, ParseRSUGPS)
     {
         std::string gps_nmea_data = "$GPGGA,142440.00,3857.3065,N,07708.9734,W,2,18,0.65,86.18,M,-34.722,M,,*62";
         auto gps_map = _rsuWorker->ParseRSUGPS(gps_nmea_data);
-        ASSERT_EQ(1, gps_map.size());
+        EXPECT_EQ(1, gps_map.size());
         double expected_latitude = 38.9551;
         double expected_longitude = -77.1496;
         for (auto itr = gps_map.begin(); itr != gps_map.end(); itr++)
         {
-            ASSERT_NEAR(expected_latitude, itr->first, 0.001);
-            ASSERT_NEAR(expected_longitude, itr->second, 0.001);
+            EXPECT_NEAR(expected_latitude, itr->first, 0.001);
+            EXPECT_NEAR(expected_longitude, itr->second, 0.001);
         }
         std::string invalid_gps_nmea_data = "$*GPGGA,invalid";
         auto gps_map_invalid = _rsuWorker->ParseRSUGPS(invalid_gps_nmea_data);
-        ASSERT_EQ(0, gps_map_invalid.size());
+        EXPECT_EQ(0, gps_map_invalid.size());
     }
 
     TEST_F(test_RSUHealthMonitorWorker, getRSUStatus)
     {
         uint16_t port = 161;
-        auto json = _rsuWorker->getRSUStatus(tmx::utils::rsu::RSU_SPEC::NTCIP_1218, "127.0.0.1", port, "testUser", "SHA-512", "testtesttest", "AES-256", "test1234", "authPriv", 1000);
-        ASSERT_TRUE(json.empty());
+        EXPECT_THROW(_rsuWorker->getRSUStatus(tmx::utils::rsu::RSU_SPEC::NTCIP_1218, "127.0.0.1", port, "testUser", "SHA-512", "testtesttest", "AES-256", "test1234", "authPriv", 1000), std::runtime_error);
 
-        json = _rsuWorker->getRSUStatus(tmx::utils::rsu::RSU_SPEC::NTCIP_1218, "127.0.0.1", port, "testUser", "SHA-512", "test1234", "AES-256", "test1234", "authPriv", 1000);
-        ASSERT_TRUE(json.empty());
+        EXPECT_THROW(_rsuWorker->getRSUStatus(tmx::utils::rsu::RSU_SPEC::NTCIP_1218, "127.0.0.1", port, "testUser", "SHA-512", "test1234", "AES-256", "test1234", "authPriv", 1000), std::runtime_error);
 
-        json = _rsuWorker->getRSUStatus(tmx::utils::rsu::RSU_SPEC::NTCIP_1218, "127.0.0.1", port, "testUser", "SHA-512", "test1234", "AES-256", "test1234", "authPriv", 1000);
-        ASSERT_TRUE(json.empty());
+        EXPECT_THROW( _rsuWorker->getRSUStatus(tmx::utils::rsu::RSU_SPEC::NTCIP_1218, "127.0.0.1", port, "testUser", "SHA-512", "test1234", "AES-256", "test1234", "authPriv", 1000), std::runtime_error);
+
     }
 
     TEST_F(test_RSUHealthMonitorWorker, convertJsonToTMXMsg)
@@ -68,7 +68,7 @@ namespace RSUHealthMonitor
         json["rsuMode"] = 4;
         auto rsuStatusTmxMsg = _rsuWorker->convertJsonToTMXMsg(json);
         string expectedStr = "{\"rsuID\":\"RSU4.1\",\"rsuMode\":4}\n";
-        ASSERT_EQ(expectedStr, rsuStatusTmxMsg.to_string());
+        EXPECT_EQ(expectedStr, rsuStatusTmxMsg.to_string());
     }
 
     TEST_F(test_RSUHealthMonitorWorker, populateJson)
@@ -84,8 +84,8 @@ namespace RSUHealthMonitor
         auto json = _rsuWorker->populateJson("rsuGpsOutputString", stringObj);
         double expected_latitude = 38.9551;
         double expected_longitude = -77.1496;
-        ASSERT_NEAR(expected_latitude, json["rsuGpsOutputStringLatitude"].asDouble(), 0.001);
-        ASSERT_NEAR(expected_longitude, json["rsuGpsOutputStringLongitude"].asDouble(), 0.001);
+        EXPECT_NEAR(expected_latitude, json["rsuGpsOutputStringLatitude"].asDouble(), 0.001);
+        EXPECT_NEAR(expected_longitude, json["rsuGpsOutputStringLongitude"].asDouble(), 0.001);
         for(const auto& key: json.getMemberNames())
         {
             rsuStatusJson[key] = json[key];
@@ -96,15 +96,15 @@ namespace RSUHealthMonitor
         intObj.val_int = 4;
 
         json = _rsuWorker->populateJson("rsuMode", intObj);
-        ASSERT_EQ(4, json["rsuMode"].asInt64());
+        EXPECT_EQ(4, json["rsuMode"].asInt64());
         rsuStatusJson["rsuMode"] = json["rsuMode"];
 
         Json::FastWriter fasterWirter;
         string json_str = fasterWirter.write(rsuStatusJson);
         string expectedStr = "{\"rsuGpsOutputString\":\"$GPGGA,142440.00,3857.3065,N,07708.9734,W,2,18,0.65,86.18,M,-34.722,M,,*62\",\"rsuGpsOutputStringLatitude\":38.955108330000002,\"rsuGpsOutputStringLongitude\":-77.149556669999996,\"rsuMode\":4}\n";
-        ASSERT_EQ(expectedStr, json_str);
-        ASSERT_EQ(4, _rsuWorker->getJsonKeys(rsuStatusJson).size());
-        ASSERT_EQ(1, _rsuWorker->getJsonKeys(json).size());
+        EXPECT_EQ(expectedStr, json_str);
+        EXPECT_EQ(4, _rsuWorker->getJsonKeys(rsuStatusJson).size());
+        EXPECT_EQ(1, _rsuWorker->getJsonKeys(json).size());
     }
 
 }
