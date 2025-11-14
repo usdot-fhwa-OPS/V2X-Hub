0d88255ec9091ab6b8c43f1d735fc2c4fce38134
paulbourelly999 <paul.k.bourelly@leidos.com>
2025-11-14 12:39:58 -0500

    Add unit tests for 2016 TIM files

--- src/v2i-hub/TimPlugin/test/TestTimUtils.cpp
+++ src/v2i-hub/TimPlugin/test/TestTimUtils.cpp
@@ -6,7 +6,12 @@ namespace TimPlugin {
 
     TEST(TestTimUtils, isTimActiveFalse ) {
         // Test expired TIM 
+#if SAEJ2735_SPEC >= 2024
         auto tim = readTimFile("../../TimPlugin/test/test_files/tim_2024.xml");
+#else
+        auto tim = readTimFile("../../TimPlugin/test/test_files/tim_2016.xml");
+#endif
+
         EXPECT_FALSE(isTimActive(tim));
         // Start time 2025 May 14 5:36 PM (UTC)
         // Duration is 5760 miutes -> 4 days
@@ -15,7 +20,11 @@ namespace TimPlugin {
 
       TEST(TestTimUtils, isTimActivePersistTrue ) {
         // Test Persist TIM 
+#if SAEJ2735_SPEC >= 2024
         auto tim = readTimFile("../../TimPlugin/test/test_files/tim_2024.xml");
+#else
+        auto tim = readTimFile("../../TimPlugin/test/test_files/tim_2016.xml");
+#endif
         auto timPtr = tim->get_j2735_data();
         // Setting duration time to max value 32000 should indicate indefinite broadcast of TIM
         timPtr->dataFrames.list.array[0]->durationTime = 32000;
@@ -27,7 +36,11 @@ namespace TimPlugin {
 
     TEST(TestTimUtils, isTimActiveEdgeCases) {
         // Test TIM when start time == current time 
+#if SAEJ2735_SPEC >= 2024
         auto tim = readTimFile("../../TimPlugin/test/test_files/tim_2024.xml");
+#else
+        auto tim = readTimFile("../../TimPlugin/test/test_files/tim_2016.xml");
+#endif
         auto timPtr = tim->get_j2735_data();
         // Set start time to current time 
         // 1) Get current time 
@@ -77,6 +90,7 @@ namespace TimPlugin {
     }
 
     TEST(TestTimUtils, readTimXml) {
+#if SAEJ2735_SPEC >= 2024
         std::string timXml = R"(
             <TravelerInformation>
                 <msgCnt>1</msgCnt>
@@ -217,7 +231,148 @@ namespace TimPlugin {
                 </dataFrames>
             </TravelerInformation>
         )";
-
+#else
+        std::string timXml = R"(
+            <TravelerInformation>
+                <msgCnt>1</msgCnt>
+                <packetID>0000000000087FAA72</packetID>
+                <dataFrames>
+                    <TravelerDataFrame>
+                    <sspTimRights>0</sspTimRights>
+                    <frameType>
+                        <roadSignage/>
+                    </frameType>
+                    <msgId>
+                        <roadSignID>
+                        <position>
+                            <lat>281185423</lat>
+                            <long>-818311882</long>
+                            <elevation>220</elevation>
+                        </position>
+                        <viewAngle>1100000000000001</viewAngle>
+                        <mutcdCode>
+                            <maintenance/>
+                        </mutcdCode>
+                        </roadSignID>
+                    </msgId>
+                    <startYear>2025</startYear>
+                    <startTime>5760</startTime>
+                    <duratonTime>5760</duratonTime>
+                    <priority>5</priority>
+                    <sspLocationRights>0</sspLocationRights>
+                    <regions>
+                        <GeographicalPath>
+                        <anchor>
+                            <lat>281185423</lat>
+                            <long>-818311882</long>
+                            <elevation>220</elevation>
+                        </anchor>
+                        <laneWidth>366</laneWidth>
+                        <directionality>
+                            <forward/>
+                        </directionality>
+                        <closedPath>
+                            <true/>
+                        </closedPath>
+                        <direction>1100000000000001</direction>
+                        <description>
+                            <path>
+                            <offset>
+                                <xy>
+                                <nodes>
+                                    <NodeXY>
+                                    <delta>
+                                        <node-XY3>
+                                        <x>277</x>
+                                        <y>-1815</y>
+                                        </node-XY3>
+                                    </delta>
+                                    </NodeXY>
+                                    <NodeXY>
+                                    <delta>
+                                        <node-XY2>
+                                        <x>20</x>
+                                        <y>885</y>
+                                        </node-XY2>
+                                    </delta>
+                                    </NodeXY>
+                                    <NodeXY>
+                                    <delta>
+                                        <node-XY4>
+                                        <x>-296</x>
+                                        <y>2261</y>
+                                        </node-XY4>
+                                    </delta>
+                                    </NodeXY>
+                                    <NodeXY>
+                                    <delta>
+                                        <node-XY3>
+                                        <x>-527</x>
+                                        <y>1756</y>
+                                        </node-XY3>
+                                    </delta>
+                                    <attributes>
+                                        <dElevation>-10</dElevation>
+                                    </attributes>
+                                    </NodeXY>
+                                    <NodeXY>
+                                    <delta>
+                                        <node-XY1>
+                                        <x>-408</x>
+                                        <y>-66</y>
+                                        </node-XY1>
+                                    </delta>
+                                    </NodeXY>
+                                    <NodeXY>
+                                    <delta>
+                                        <node-XY3>
+                                        <x>527</x>
+                                        <y>-1652</y>
+                                        </node-XY3>
+                                    </delta>
+                                    </NodeXY>
+                                    <NodeXY>
+                                    <delta>
+                                        <node-XY2>
+                                        <x>26</x>
+                                        <y>-826</y>
+                                        </node-XY2>
+                                    </delta>
+                                    <attributes>
+                                        <dElevation>10</dElevation>
+                                    </attributes>
+                                    </NodeXY>
+                                    <NodeXY>
+                                    <delta>
+                                        <node-XY3>
+                                        <x>329</x>
+                                        <y>-1186</y>
+                                        </node-XY3>
+                                    </delta>
+                                    </NodeXY>
+                                </nodes>
+                                </xy>
+                            </offset>
+                            </path>
+                        </description>
+                        </GeographicalPath>
+                    </regions>
+                    <sspMsgRights1>0</sspMsgRights1>
+                    <sspMsgRights2>0</sspMsgRights2>
+                    <content>
+                        <workZone>
+                        <SEQUENCE>
+                            <item>
+                            <itis>769</itis>
+                            </item>
+                        </SEQUENCE>
+                        </workZone>
+                    </content>
+                    </TravelerDataFrame>
+                </dataFrames>
+            </TravelerInformation>
+        )"
+#endif
         auto tim = readTimXml(timXml);
         auto timPtr = tim->get_j2735_data();
         EXPECT_EQ(2025, *(timPtr->dataFrames.list.array[0]->startYear));
@@ -229,7 +384,11 @@ namespace TimPlugin {
     }
 
     TEST(TestTimUtils, readTimFile) {
+#if SAEJ2735_SPEC >= 2024
         auto tim = readTimFile("../../TimPlugin/test/test_files/tim_2024.xml");
+#else
+        auto tim = readTimFile("../../TimPlugin/test/test_files/tim_2016.xml");
+#endif
         auto timPtr = tim->get_j2735_data();
         EXPECT_EQ(2025, *(timPtr->dataFrames.list.array[0]->startYear));
         EXPECT_EQ(181181, timPtr->dataFrames.list.array[0]->startTime);
@@ -239,7 +398,11 @@ namespace TimPlugin {
         EXPECT_THROW(readTimFile("../../TimPlugin/test/test_files/non-existant-file.xml"), tmx::TmxException);
 
         // Attempt to read from file that has the incorrect file extension
+#if SAEJ2735_SPEC >= 2024
         EXPECT_NO_THROW(readTimFile("../../TimPlugin/test/test_files/tim_2024.bad_ext"));
+#else
+        EXPECT_NO_THROW(readTimFile("../../TimPlugin/test/test_files/tim_2016.bad_ext"));
+#endif
     }
 }
 
