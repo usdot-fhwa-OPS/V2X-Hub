18be86c4df2d4d7b04d7f0cde1ff1647883d42e0
paulbourelly999 <77466294+paulbourelly999@users.noreply.github.com>
2026-04-28 14:15:46 -0400

    Update generate_local_trusted_certificates.sh

--- configuration/generate_local_trusted_certificates.sh
+++ configuration/generate_local_trusted_certificates.sh
@@ -3,7 +3,7 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Load .env if present to get V2XHUB_VOLUME_PATH
-if [ -f ".env" ]; then
+if [ -f "${SCRIPT_DIR}/.env" ]; then
   source "${SCRIPT_DIR}/.env"
 fi
 # Ensure required environment variable is set
