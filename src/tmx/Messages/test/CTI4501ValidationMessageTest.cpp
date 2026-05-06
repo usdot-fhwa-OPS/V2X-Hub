2ed4322c196bc799fe50b581b8e1e4cc782c2e32
Satish Natarajan <natarajan.sa@northeastern.edu>
2026-05-06 17:49:22 +0000

    Forgot to remove private:

--- src/tmx/Messages/test/CTI4501ValidationMessageTest.cpp
+++ src/tmx/Messages/test/CTI4501ValidationMessageTest.cpp
@@ -1,14 +1,18 @@
 #include <gtest/gtest.h>
 #include "CTI4501ValidationMessage.h"
 
-namespace tmx::messages{
-    class CTI4501ValidationMessageTest : public testing::Test{
-        protected:  
-        std::shared_ptr<CTI4501ValidationMessage> tmxCti4501Ptr; 
-        CTI4501ValidationMessageTest(){
+namespace tmx::messages
+{
+    class CTI4501ValidationMessageTest : public testing::Test
+    {
+    protected:
+        std::shared_ptr<CTI4501ValidationMessage> tmxCti4501Ptr;
+        CTI4501ValidationMessageTest()
+        {
             tmxCti4501Ptr = std::make_shared<CTI4501ValidationMessage>();
-        }         
-        void SetUp() override {
+        }
+        void SetUp() override
+        {
             tmxCti4501Ptr->set_eventGeneratedAt(12222222222);
             tmxCti4501Ptr->set_eventType("SpatMinimumData");
             tmxCti4501Ptr->set_intersectionID(123);
@@ -16,15 +20,13 @@ namespace tmx::messages{
             tmxCti4501Ptr->set_source("RSU");
             ProcessingTimePeriod timePeriod(11111111111, 12222222222);
             tmxCti4501Ptr->set_timePeriod(timePeriod);
-            std::vector<MissingDataElement> elements { MissingDataElement("id"), MissingDataElement("minEndTime") };
+            std::vector<MissingDataElement> elements{MissingDataElement("id"), MissingDataElement("minEndTime")};
             tmxCti4501Ptr->set_missingDataElements(elements);
         }
-
-        private:
-            
     };
 
-    TEST_F(CTI4501ValidationMessageTest, attributes){    
+    TEST_F(CTI4501ValidationMessageTest, attributes)
+    {
         EXPECT_EQ(12222222222, tmxCti4501Ptr->get_eventGeneratedAt());
         EXPECT_EQ("SpatMinimumData", tmxCti4501Ptr->get_eventType());
         EXPECT_EQ(123, tmxCti4501Ptr->get_intersectionID());
@@ -37,12 +39,14 @@ namespace tmx::messages{
         EXPECT_EQ("minEndTime", tmxCti4501Ptr->get_missingDataElements()[1].value);
     }
 
-    TEST_F(CTI4501ValidationMessageTest, to_string){
+    TEST_F(CTI4501ValidationMessageTest, to_string)
+    {
         std::string expectedStr = "{\"eventGeneratedAt\":\"12222222222\",\"eventType\":\"SpatMinimumData\",\"intersectionID\":\"123\",\"roadRegulatorID\":\"456\",\"source\":\"RSU\",\"timePeriod\":{\"beginTimestamp\":\"11111111111\",\"endTimestamp\":\"12222222222\"},\"missingDataElements\":[\"id\",\"minEndTime\"]}\n";
         EXPECT_EQ(expectedStr, tmxCti4501Ptr->to_string());
     }
 
-    TEST_F(CTI4501ValidationMessageTest, deserialize){
+    TEST_F(CTI4501ValidationMessageTest, deserialize)
+    {
         auto tmxCti4501Ptr2 = std::make_shared<CTI4501ValidationMessage>();
         std::string expectedStr = R"(
                                 {
