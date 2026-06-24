b32b31ae3713a47e82d3e774e9e9c875cfca1e17
paulbourelly999 <paul.k.bourelly@leidos.com>
2026-06-24 15:29:45 -0400

    Updates

--- scripts/deployment_dependencies.sh
+++ scripts/deployment_dependencies.sh
@@ -8,8 +8,8 @@ set -ex
 
 # add the STOL APT repository
 echo "deb [trusted=yes] http://s3.amazonaws.com/stol-apt-repository develop ${DISTRIB_CODENAME}" > /etc/apt/sources.list.d/stol-apt-repository.list
-apt-get clean
-apt-get update --fix-missing
+apt clean
+apt update --fix-missing
 
 # NOTE: libwebsockets-dev from Ubuntu 20 on is sufficient
 DEPENDENCIES="ca-certificates \
@@ -49,4 +49,4 @@ LIBRARY_DEPENDENCIES=" \
 
 # install all things needed for deployment, always done
 # TODO: --install-no-recommends
-apt-get install --install-no-recommends -y  $DEPENDENCIES ${LIBRARY_DEPENDENCIES}
+apt install --install-no-recommends -y  $DEPENDENCIES ${LIBRARY_DEPENDENCIES}
