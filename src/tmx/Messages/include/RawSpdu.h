46734859df276069c4c23e0b65fe259f9975a9af
Mahbubul Alam Palash <palashm@leidos.com>
2026-07-09 13:04:36 -0400

    dt4it-105: Added custom RawSPDU for processing Signed Messages
    <!-- Thanks for the contribution, this is awesome. -->
    
    # PR Details
    ## Description
    
    1. Added new TMX message SecuredV2XMessage to support signed V2X
    messages
    2. Added Unit tests for newly added message
    
    
    
    ## Related Jira Key
    
    [DT4IT-105](https://usdot-carma.atlassian.net/jira/software/c/projects/DT4IT/boards/591/backlog?selectedIssue=DT4IT-105)
    
    
    
    ## How Has This Been Tested?
    Added unit tests and tested by running test task in dev container 
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Defect fix (non-breaking change that fixes an issue)
    - [x] New feature (non-breaking change that adds functionality)
    - [ ] Breaking change (fix or feature that cause existing functionality
    to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have added any new packages to the sonar-scanner.properties file
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [ ] I have read the
    [**CONTRIBUTING**](https://github.com/usdot-fhwa-stol/carma-platform/blob/develop/Contributing.md)
    document.
    - [x] I have added tests to cover my changes.
    - [x] All new and existing tests passed.
    
    
    [DT4IT-105]:
    https://usdot-carma.atlassian.net/browse/DT4IT-105?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ src/tmx/Messages/include/RawSpdu.h
@@ -0,0 +1,61 @@
+/*
+ * RawSpdu.h
+ */
+#ifndef INCLUDE__RawSpdu _H_
+#define INCLUDE__RawSpdu _H_
+#include <tmx/messages/message.hpp>
+#include <tmx/messages/byte_stream.hpp>
+#include <memory>
+#include <vector>
+#include <cstdint>
+#include "MessageTypes.h"
+namespace tmx
+{
+    namespace messages
+    {
+        /**
+         * RawSpdu - TMX message wrapper carrying a raw, encoded SPDU.
+         *
+         * Holds an already-encoded 1609.2-secured J2735 message, such as BSM, PSM,
+         * MAP, SPaT, etc., to move raw V2X payloads across the TMX bus.
+         *
+         * Fields (none are set automatically; the caller must populate them before publishing):
+         *   - spdu_data:    Raw SPDU bytes (stored as a std::vector<uint8_t>).
+         *                   Use set_spdu_data()/get_spdu_data(), or the byte-vector
+         *                   interface via set_spdu_data_bytes()/get_spdu_data_bytes().
+         *   - uuid:         Unique identifier for this packet (stored as a
+         *                   std::vector<uint8_t>), used for correlation and tracing.
+         *                   Use set_uuid()/get_uuid().
+         *   - MessageType:  J2735 type label, e.g., "BSM", "PSM", or "MAP".
+         *   - timestamp_ms: Epoch timestamp in milliseconds, received or generated time
+         *                   depending on the producer. Defaults to 0 if unset.
+         *   - psid:         1609.2 PSID (Protocol Service Identifier),specified as an integer.
+         */
+        class RawSpdu : public tmx::message
+        {
+        public:
+            RawSpdu() {}
+            #if __cplusplus > 199711L
+                /// Message type for routing this message through TMX core.
+                static constexpr const char* MessageType = MSGTYPE_DECODED_STRING;
+
+                /// Message sub type for routing this message through TMX core.
+                static constexpr const char* MessageSubType = MSGSUBTYPE_BASIC_STRING;
+            #endif
+            RawSpdu(const tmx::message_container_type &contents) : tmx::message(contents) {}
+            // Full SPDU bytes
+                std_attribute(this->msg, tmx::byte_stream, spduData, tmx::byte_stream(), )
+                // Packet UUID
+                std_attribute(this->msg, tmx::byte_stream, uuid, tmx::byte_stream(), )
+
+                // J2735 payload type, e.g. "BSM", "PSM", "MAP".
+                std_attribute(this->msg, std::string, messageType, "", )
+                // timestamp
+                std_attribute(this->msg, int64_t, timestampMs, 0, )
+                /// 1609Dot2 PSID.
+                std_attribute(this->msg, int, psid, 0, )
+        
+        };
+    }
+}
+#endif /* INCLUDE__RawSpdu _H_ */
---
+++ src/tmx/Messages/test/RawSpduTest.cpp
@@ -0,0 +1,96 @@
+#include <gtest/gtest.h>
+#include "RawSpdu.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+using namespace tmx;
+using namespace tmx::messages;
+
+static const std::string bsmHex =
+    "001425067c0eb5842562e66e8a2b9ea6c96408b97fffffff900027d9637d07d0007fff8000640fa0";
+
+// PSM Payload
+static const std::string psmHex =
+    "00201c000002a5158048d159e14cdd338f3d4da420101effffffff00000000";
+
+
+static const std::string uuidHex = "0a303030303fdfa7";
+
+
+TEST(RawSpduTest, SetAndGetAttributesBsm) {
+    RawSpdu msg;
+
+    tmx::byte_stream spdu = tmx::byte_stream_decode(bsmHex);
+    tmx::byte_stream uuid = tmx::byte_stream_decode(uuidHex);
+
+    msg.set_spduData(spdu);
+    msg.set_uuid(uuid);
+    msg.set_messageType("BSM"); // BasicSafetyMessage
+    msg.set_timestampMs(1718900000000);
+    msg.set_psid(8002);
+
+
+
+    std::cout << msg.to_string() << std::endl;
+
+    EXPECT_EQ("BSM", msg.get_messageType());
+    EXPECT_EQ(1718900000000, msg.get_timestampMs());
+
+
+    auto spduOut = msg.get_spduData();
+    EXPECT_EQ(spdu, spduOut);
+
+    auto uuidOut = msg.get_uuid();
+    EXPECT_EQ(uuid, uuidOut);
+    EXPECT_EQ(8002, msg.get_psid());
+
+}
+
+TEST(RawSpduTest, ToStringSerializesByteStreamAsHexString)
+{
+    RawSpdu msg;
+
+    tmx::byte_stream spdu = tmx::byte_stream_decode(bsmHex);
+    tmx::byte_stream uuid = tmx::byte_stream_decode(uuidHex);
+
+    msg.set_spduData(spdu);
+    msg.set_uuid(uuid);
+    msg.set_messageType("BSM");
+    msg.set_timestampMs(1718900000000);
+    msg.set_psid(8002);
+
+    std::string json = msg.to_string();
+
+    EXPECT_NE(json.find("\"spduData\":\"" + bsmHex + "\""), std::string::npos);
+    EXPECT_NE(json.find("\"uuid\":\"" + uuidHex + "\""), std::string::npos);
+    EXPECT_NE(json.find("\"messageType\":\"BSM\""), std::string::npos);
+    EXPECT_NE(json.find("\"timestampMs\":\"1718900000000\""), std::string::npos);
+    EXPECT_NE(json.find("\"psid\":\"8002\""), std::string::npos);
+}
+
+TEST(RawSpduTest, SetAndGetAttributesPsm) {
+    RawSpdu msg;
+
+    tmx::byte_stream spdu = tmx::byte_stream_decode(psmHex);
+    tmx::byte_stream uuid = tmx::byte_stream_decode(uuidHex);
+
+    msg.set_spduData(spdu);
+    msg.set_uuid(uuid);
+    msg.set_messageType("PSM"); // PersonalSafetyMessage
+    msg.set_timestampMs(1718900123456);
+
+    msg.set_psid(8001);
+    std::cout << msg.to_string() << std::endl;
+
+    EXPECT_EQ("PSM", msg.get_messageType());
+    EXPECT_EQ(1718900123456, msg.get_timestampMs());
+
+    auto spduOut = msg.get_spduData();
+    EXPECT_EQ(spdu, spduOut);
+
+    auto uuidOut = msg.get_uuid();
+    EXPECT_EQ(uuid, uuidOut);
+    EXPECT_EQ(8001, msg.get_psid());
+}
+
