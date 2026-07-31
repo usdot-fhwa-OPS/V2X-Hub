f08e7b93f67e0d2442269328e431be2f7a0670af
radishco <satnat01@gmail.com>
2026-07-31 17:00:30 +0000

    Added todo comment

--- src/v2i-hub/IntersectionValidationPlugin/src/IntersectionValidationPlugin.h
+++ src/v2i-hub/IntersectionValidationPlugin/src/IntersectionValidationPlugin.h
@@ -64,7 +64,7 @@ namespace IntersectionValidation
         uint spatValidationPassed = 0;
         uint mapFieldValidationErrors = 0;
         uint mapValidationPassed = 0;
-        std::string rsuSource;
+        std::string rsuSource; // TODO: Instead of setting the rsu IP here, have the message receiver grab the IP and attach it to the message
 
         uint spatRevisionPassed = 0;
         uint mapRevisionPassed = 0;
