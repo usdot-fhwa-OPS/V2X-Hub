fff7caf438c56a4b14dd16f8dd847d5a7590362a
paulbourelly999 <paul.k.bourelly@leidos.com>
2026-09-22 18:48:59 -0400

    Undo changes

--- src/v2i-hub/ODEForwardPlugin/src/ODEForwardPlugin.h
+++ src/v2i-hub/ODEForwardPlugin/src/ODEForwardPlugin.h
@@ -17,7 +17,7 @@
 #ifndef TMX_PLUGINS_ODEForwardPlugin_H_
 #define TMX_PLUGINS_ODEForwardPlugin_H_
 
-#include <PluginClientClockAware.h>
+#include <PluginClient.h>
 #include <iostream>
 #include <cstring>
 #include <string>
@@ -58,7 +58,7 @@ namespace ODEForwardPlugin
 	/**
 	 * This plugin logs the BSM messages received in the following CSV format.
 	 */
-	class ODEForwardPlugin: public tmx::utils::PluginClientClockAware
+	class ODEForwardPlugin: public tmx::utils::PluginClient
 	{
 		public:
 			explicit ODEForwardPlugin(const std::string &name);
